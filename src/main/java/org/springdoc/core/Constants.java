c2b7b94ddb4009b6520ab019a9b25ea6892e77ad
bnasslahsen <badr.nasslahsen@gmail.com>
2019-07-12 02:16:02 +0200

    rename groupid

---
+++ src/main/java/org/springdoc/core/Constants.java
@@ -0,0 +1,23 @@
+package org.springdoc.core;
+
+public class Constants {
+
+	public static final String GET_METHOD = "get";
+	public static final String POST_METHOD = "post";
+	public static final String PUT_METHOD = "put";
+	public static final String DELETE_METHOD = "delete";
+	public static final String PATCH_METHOD = "patch";
+	public static final String TRACE_METHOD = "trace";
+	public static final String HEAD_METHOD = "head";
+	public static final String OPTIONS_METHOD = "options";
+
+	public static String QUERY_PARAM = "query";
+	public static String HEADER_PARAM = "header";
+	public static String COOKIE_PARAM = "cookie";
+	public static String PATH_PARAM = "path";
+	public static String FORM_PARAM = "form";
+
+	public static final String DEFAULT_DESCRIPTION = "default response";
+	public static final String DEFAULT_OPERATION_DESCRIPTION = "default description";
+
+}
---
+++ src/main/java/org/springdoc/core/SwaggerProperties.java
@@ -0,0 +1,19 @@
+package org.springdoc.core;
+
+public interface SwaggerProperties {
+	String DEFAULT_PROPS_LOCATION = "/swagger.properties";
+	String DEFAULT_LICENSE_VALUE = "Apache 2.0 License";
+	String DEFAULT_LICENSE_URL = "http://www.apache.org/licenses/LICENSE-2.0.html";
+
+	String RESOURCE_PACKAGE_PROPERTY = "resource.package";
+	String TITLE_PROPERTY = "title";
+	String VERSION_PROPERTY = "version";
+	String DESCRIPTION_PROPERTY = "description";
+	String CONTACT_PROPERTY = "contact";
+	String LICENSE_PROPERTY = "license";
+	String LICENSE_URL_PROPERTY = "license.url";
+	String TERMS_URL_PROPERTY = "terms.url";
+	String PRETTY_PRINT_PROPERTY = "pretty.print";
+	String FILTER_CLASS_PROPERTY = "filter.class";
+
+}
