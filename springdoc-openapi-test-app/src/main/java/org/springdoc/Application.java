c228c483fcd15980d371a2145cdf6b3e7ecca282
sprigndoc <springdoc99@gmail.com>
2019-07-13 22:27:53 +0200

    tomcat deploy

--- springdoc-openapi-test-app/src/main/java/org/springdoc/Application.java
+++ springdoc-openapi-test-app/src/main/java/org/springdoc/Application.java
@@ -2,11 +2,12 @@ package org.springdoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "org.springdoc.sample1", "org.springdoc.sample2" })
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
