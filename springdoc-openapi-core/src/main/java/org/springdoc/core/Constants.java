d227bd1e4fa872d000257d1974df311a7ae28e4f
sprigndoc <springdoc99@gmail.com>
2019-07-14 23:12:11 +0200

    project clean up

--- springdoc-openapi-core/src/main/java/org/springdoc/core/Constants.java
+++ springdoc-openapi-core/src/main/java/org/springdoc/core/Constants.java
@@ -18,20 +18,10 @@ public final class Constants {
 	public static final String FORM_PARAM = "form";
 
 	public static final String DEFAULT_DESCRIPTION = "default response";
+	public static final String DEFAULT_TITLE = "OpenAPI definition";
+	public static final String DEFAULT_VERSION = "v0";
 
 	public static final String DEFAULT_PROPS_LOCATION = "/swagger.properties";
 	public static final String DEFAULT_LICENSE_VALUE = "Apache 2.0 License";
 	public static final String DEFAULT_LICENSE_URL = "http://www.apache.org/licenses/LICENSE-2.0.html";
-
-	// SWAGGER PROPERTIES
-	public static final String RESOURCE_PACKAGE_PROPERTY = "resource.package";
-	public static final String TITLE_PROPERTY = "title";
-	public static final String VERSION_PROPERTY = "version";
-	public static final String DESCRIPTION_PROPERTY = "description";
-	public static final String CONTACT_PROPERTY = "contact";
-	public static final String LICENSE_PROPERTY = "license";
-	public static final String LICENSE_URL_PROPERTY = "license.url";
-	public static final String TERMS_URL_PROPERTY = "terms.url";
-	public static final String PRETTY_PRINT_PROPERTY = "pretty.print";
-	public static final String FILTER_CLASS_PROPERTY = "filter.class";
 }
--- springdoc-openapi-core/src/main/java/org/springdoc/core/InfoBuilder.java
+++ springdoc-openapi-core/src/main/java/org/springdoc/core/InfoBuilder.java
@@ -1,27 +1,65 @@
 package org.springdoc.core;
 
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import static org.springdoc.core.Constants.DEFAULT_TITLE;
+import static org.springdoc.core.Constants.DEFAULT_VERSION;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
+import org.springframework.stereotype.Component;
+
+import io.swagger.v3.core.util.AnnotationsUtils;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.info.Info;
+
+@Component
 public class InfoBuilder {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(InfoBuilder.class);
+
+	@Autowired
+	private ApplicationContext context;
 
 	private InfoBuilder() {
 		super();
 	}
 
-	public static Info build() {
-		final Info info = new Info().title("Simple API").version("my version").description("This is a simple API")
-				.termsOfService("termsOfService").version("1.0.0")
-				.contact(new Contact().name("contact-name").email("you@your-company.com").url("compan-myUrl"));
-
-		if (info.getLicense() == null) {
-			License license = new License();
-			license.setName(Constants.DEFAULT_LICENSE_VALUE);
-			license.setUrl(Constants.DEFAULT_LICENSE_URL);
-			info.license(license);
+	public Info build() throws ClassNotFoundException {
+		ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+		scanner.addIncludeFilter(new AnnotationTypeFilter(OpenAPIDefinition.class));
+		List<String> packagesToScan = AutoConfigurationPackages.get(context);
+		OpenAPIDefinition apiDef = getApiDefClass(scanner, packagesToScan);
+		Info info = null;
+		if (apiDef != null) {
+			info = AnnotationsUtils.getInfo(apiDef.info()).orElse(null);
+		} else {
+			info = new Info().title(DEFAULT_TITLE).version(DEFAULT_VERSION);
 		}
 		return info;
+	}
+
+	private OpenAPIDefinition getApiDefClass(ClassPathScanningCandidateComponentProvider scanner,
+			List<String> packagesToScan) {
+		for (String pack : packagesToScan) {
+			for (BeanDefinition bd : scanner.findCandidateComponents(pack)) {
+				// first one found is ok
+				try {
+					return AnnotationUtils.findAnnotation(Class.forName(bd.getBeanClassName()),
+							OpenAPIDefinition.class);
+				} catch (ClassNotFoundException e) {
+					LOGGER.error("Class Not Found in classpath: " + e.getMessage());
+				}
+			}
+		}
+		return null;
 	}
 
 }
