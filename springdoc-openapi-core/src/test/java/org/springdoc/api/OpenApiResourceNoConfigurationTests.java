8746138b8abdedd189359968d50a3f4e444f4942
Bruno Mendola <1853562+brunomendola@users.noreply.github.com>
2019-07-29 18:21:24 +0200

    Unit tests

---
+++ springdoc-openapi-core/src/test/java/org/springdoc/api/OpenApiResourceNoConfigurationTests.java
@@ -0,0 +1,45 @@
+package org.springdoc.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springdoc.SpringDocSpringDocTestApplication;
+import org.springdoc.config.SpringDocConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringDocSpringDocTestApplication.class)
+@ActiveProfiles("test")
+@TestPropertySource(properties = "springdoc.api-docs.path=/api-docs")
+@AutoConfigureMockMvc
+public class OpenApiResourceNoConfigurationTests {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  /**
+   * should return
+   * {"openapi":"3.0.1","info":{"title":"OpenAPI definition","version":"v0"},"paths":{},"components":{}}
+   */
+  @Test
+  public void givenNoConfiguration_whenGetApiJson_returnsDefaultEmptyDocs() throws Exception {
+    mockMvc
+        .perform(get("/api-docs"))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.openapi", is("3.0.1")))
+        .andExpect(jsonPath("$.info.title", is("OpenAPI definition")))
+        .andExpect(jsonPath("$.info.version", is("v0")))
+        .andExpect(jsonPath("$.paths").isEmpty())
+        .andExpect(jsonPath("$.components").isEmpty());
+  }
+}
