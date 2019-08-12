2f66740d020bd2db708940788c27a330f8873950
springdoc <springdoc99@gmail.com>
2019-08-12 03:59:13 +0200

    fixes #16

--- springdoc-openapi-webflux-core/src/main/java/org/springdoc/api/OpenApiResource.java
+++ springdoc-openapi-webflux-core/src/main/java/org/springdoc/api/OpenApiResource.java
@@ -17,6 +17,7 @@ import org.springdoc.core.OpenAPIBuilder;
 import org.springdoc.core.OperationBuilder;
 import org.springdoc.core.TagsBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -38,6 +39,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import reactor.core.publisher.Mono;
 
 @RestController
+@ConditionalOnProperty(name = SPRINGDOC_ENABLED, matchIfMissing = true)
 public class OpenApiResource extends AbstractOpenApiResource {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(OpenApiResource.class);
