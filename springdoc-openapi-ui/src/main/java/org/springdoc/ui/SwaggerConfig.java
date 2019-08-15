bac9b23f1d07257c87e955c86dfd1e9e58819e8c
springdoc <springdoc99@gmail.com>
2019-08-15 13:39:41 +0200

    project review

--- springdoc-openapi-ui/src/main/java/org/springdoc/ui/SwaggerConfig.java
+++ springdoc-openapi-ui/src/main/java/org/springdoc/ui/SwaggerConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebMvc
 public class SwaggerConfig extends WebMvcConfigurerAdapter {
