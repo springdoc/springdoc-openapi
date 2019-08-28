bb0e543ee5f7e4519c22e63cbbb25aa126c35f34
springdoc <springdoc99@gmail.com>
2019-08-28 22:00:38 +0200

    fixes #68

---
+++ springdoc-openapi-core/src/test/java/test/org/springdoc/api/app6/SpringDocTestApp6.java
@@ -0,0 +1,11 @@
+package test.org.springdoc.api.app6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocTestApp6 {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDocTestApp6.class, args);
+  }
+}
