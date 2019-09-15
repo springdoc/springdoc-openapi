0a3cb560c60beb57d28404d63209e34545a16595
springdoc <springdoc99@gmail.com>
2019-09-15 02:47:00 +0200

    project update

--- springdoc-openapi-core/src/test/java/test/org/springdoc/api/app36/HelloController.java
+++ springdoc-openapi-core/src/test/java/test/org/springdoc/api/app36/HelloController.java
@@ -1,34 +1,14 @@
 package test.org.springdoc.api.app36;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-
-@Controller
-@RequestMapping("/api/v1/poc/")
+@RestController
 public class HelloController {
 
-	@Operation(summary = "Get thing", responses = {
-			@ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
-			@ApiResponse(responseCode = "404", description = "Not found", content = @Content),
-			@ApiResponse(responseCode = "401", description = "Authentication Failure", content = @Content(schema = @Schema(hidden = true))) })
-	@RequestMapping(path = "/testme", method = RequestMethod.GET)
-	ResponseEntity<String> testme() {
-		return ResponseEntity.ok("Hello");
+	@GetMapping("/persons")
+	public String persons() {
+		return "OK";
 	}
 
-	@Operation(summary = "Get thing", responses = {
-			@ApiResponse(description = "Successful Operation", responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = String.class))),
-			@ApiResponse(responseCode = "404", description = "Not found", content = @Content(schema = @Schema(hidden = true))),
-			@ApiResponse(responseCode = "401", description = "Authentication Failure", content = @Content(schema = @Schema(hidden = true))) })
-	@RequestMapping(path = "/test", method = RequestMethod.GET)
-	ResponseEntity<String> test() {
-		return ResponseEntity.ok("Hello");
-	}
 }
