6e4408c5bb8df88094874672ff7ee5819421d1bd
springdoc <springdoc99@gmail.com>
2019-09-15 02:30:51 +0200

    project update

--- springdoc-openapi-core/src/test/java/test/org/springdoc/api/app36/SpringDocApp36Test.java
+++ springdoc-openapi-core/src/test/java/test/org/springdoc/api/app36/SpringDocApp36Test.java
@@ -1,10 +1,10 @@
 package test.org.springdoc.api.app36;
 
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.TestPropertySource;
 
 import test.org.springdoc.api.AbstractSpringDocTest;
 
-@PropertySource("classpath:/properties/36.properties")
+@TestPropertySource("classpath:/properties/36.properties")
 public class SpringDocApp36Test extends AbstractSpringDocTest {
 
 
