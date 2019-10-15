01ec5174b3aee957a486fa102beed4bb2720c456
springdoc <springdoc99@gmail.com>
2019-10-15 22:31:20 +0200

    fixes NPE

---
+++ springdoc-openapi-core/src/test/java/test/org/springdoc/api/app50/SpringDocTestApp.java
@@ -0,0 +1,35 @@
+package test.org.springdoc.api.app50;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+
+@SpringBootApplication
+public class SpringDocTestApp {
+
+	public static void main(String[] args) {
+        SpringApplication.run(SpringDocTestApp.class, args);
+    }
+
+	@Configuration
+	class OpenApiConfiguration {
+		@Bean
+		public OpenAPI defineOpenApi() {
+			OpenAPI api = new OpenAPI();
+			api.components(new Components().addResponses("Unauthorized",
+					new ApiResponse().description("Unauthorized")
+							.content(new Content().addMediaType(MediaType.APPLICATION_JSON_VALUE,
+									new io.swagger.v3.oas.models.media.MediaType().schema(new StringSchema())))));
+			return api;
+		}
+	}
+
+}
