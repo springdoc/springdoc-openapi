a92f802b893e93368acf2772e651adf64d222221
springdoc <springdoc99@gmail.com>
2019-11-18 19:56:39 +0100

    fixes #161

--- springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app18/HelloController.java
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app18/HelloController.java
@@ -5,7 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NegativeOrZero;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.PositiveOrZero;
 
 @RestController
 public class HelloController {
@@ -22,6 +25,20 @@ public class HelloController {
 
     @GetMapping(value = "/persons3")
     public String persons3(@NotBlank @Parameter(description = "persons name") @RequestParam String name) {
+        return "OK";
+    }
+
+    @GetMapping(value = "/persons4")
+    public String persons4(@PositiveOrZero int age) {
+        return "OK";
+    }
+
+    @GetMapping(value = "/persons5")
+    public String persons5(@NegativeOrZero int age) {
+        return "OK";
+    }
+    @GetMapping(value = "/persons6")
+    public String persons6(@NotEmpty @Parameter(description = "persons name") String name) {
         return "OK";
     }
 
