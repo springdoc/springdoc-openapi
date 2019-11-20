9e7262bee65667acc29f9ff8dce6d02c39958c2c
Halil İbrahim Şener <hisener@yahoo.com>
2019-11-20 15:46:54 +0100

    Make webjars prefix configurable

--- springdoc-openapi-ui/src/main/java/org/springdoc/ui/SwaggerConfig.java
+++ springdoc-openapi-ui/src/main/java/org/springdoc/ui/SwaggerConfig.java
@@ -17,13 +17,16 @@ public class SwaggerConfig extends WebMvcConfigurerAdapter { // NOSONAR
     @Value(SWAGGER_UI_PATH)
     private String swaggerPath;
 
+    @Value(WEB_JARS_PREFIX_URL)
+    private String webJarsPrefixUrl;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         String uiRootPath = "";
         if (swaggerPath.contains("/")) {
             uiRootPath = swaggerPath.substring(0, swaggerPath.lastIndexOf('/'));
         }
-        registry.addResourceHandler(uiRootPath + "/**").addResourceLocations(WEB_JARS_PREFIX_URL + "/")
+        registry.addResourceHandler(uiRootPath + "/**").addResourceLocations(webJarsPrefixUrl + "/")
                 .resourceChain(false);
     }
 }
