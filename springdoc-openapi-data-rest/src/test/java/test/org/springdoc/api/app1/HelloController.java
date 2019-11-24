bdf853e4201bdeb4c7f02ea00133af6cb1043a6b
springdoc <springdoc99@gmail.com>
2019-11-24 01:00:52 +0100

    fixes ##177

---
+++ springdoc-openapi-data-rest/src/test/java/test/org/springdoc/api/app1/HelloController.java
@@ -0,0 +1,20 @@
+package test.org.springdoc.api.app1;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/search", produces = { "application/xml", "application/json" })
+    public ResponseEntity<List<PersonDTO>> getAllPets(@NotNull Pageable pageable) {
+        System.out.println(pageable);
+        return null;
+    }
+
+}
