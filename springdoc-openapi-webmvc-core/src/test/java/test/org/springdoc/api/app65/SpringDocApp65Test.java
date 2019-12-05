98cb085447001671a12ce53ebfb9d85073e06e5e
springdoc <springdoc99@gmail.com>
2019-12-05 18:15:08 +0100

    project cleanup

--- springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app65/SpringDocApp65Test.java
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app65/SpringDocApp65Test.java
@@ -1,7 +1,5 @@
 package test.org.springdoc.api.app65;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import test.org.springdoc.api.AbstractSpringDocTest;
 
 public class SpringDocApp65Test extends AbstractSpringDocTest {
