e20c62ce42419f10159ec4a04def23d6ca3d5eaa
springdoc <springdoc99@gmail.com>
2019-12-13 00:54:00 +0100

    refactoring + initiate ability for using properties from application.yml

--- springdoc-openapi-webflux-core/src/main/java/org/springdoc/core/RequestBuilder.java
+++ springdoc-openapi-webflux-core/src/main/java/org/springdoc/core/RequestBuilder.java
@@ -1,17 +1,9 @@
 package org.springdoc.core;
 
 import io.swagger.v3.oas.models.Operation;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.context.request.NativeWebRequest;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Component
 public class RequestBuilder extends AbstractRequestBuilder {
