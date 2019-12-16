fd4b248ed59ffa29c61a16617140e44d32e7eb2c
stevenws <steven.sanjaya@gdn-commerce.com>
2019-12-16 17:09:21 +0700

    Ignore HttpRequest and HttpResponse Webflux

--- springdoc-openapi-webflux-core/src/main/java/org/springdoc/core/RequestBuilder.java
+++ springdoc-openapi-webflux-core/src/main/java/org/springdoc/core/RequestBuilder.java
@@ -1,6 +1,8 @@
 package org.springdoc.core;
 
 import io.swagger.v3.oas.models.Operation;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.server.ServerWebExchange;
@@ -15,6 +17,8 @@ public class RequestBuilder extends AbstractRequestBuilder {
 
     static {
         PARAM_TYPES_TO_IGNORE.add(ServerWebExchange.class);
+        PARAM_TYPES_TO_IGNORE.add(ServerHttpRequest.class);
+        PARAM_TYPES_TO_IGNORE.add(ServerHttpResponse.class);
     }
 
     @Override
