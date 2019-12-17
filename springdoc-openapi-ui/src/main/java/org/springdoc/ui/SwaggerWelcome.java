5e79ee3ee3846dedbf495a7ab75b8a8efa85b8e0
springdoc <springdoc99@gmail.com>
2019-12-17 01:01:34 +0100

    Added support for spring-context-indexer. fixes #262

--- springdoc-openapi-ui/src/main/java/org/springdoc/ui/SwaggerWelcome.java
+++ springdoc-openapi-ui/src/main/java/org/springdoc/ui/SwaggerWelcome.java
@@ -5,7 +5,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springdoc.core.SwaggerUiConfigProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.util.UriComponentsBuilder;
