a3252dc762d4267e485fda229d7d540988d5e23d
Anastasiia Smirnova <asmirnova@playtika.com>
2019-12-24 15:37:35 +0200

    Property `springdoc.api-docs.groups.enabled` removed, as not needed any more for enabling multiple OpenAPI definitions support

--- springdoc-openapi-ui/src/test/java/test/org/springdoc/ui/app3/SpringDocApp3Test.java
+++ springdoc-openapi-ui/src/test/java/test/org/springdoc/ui/app3/SpringDocApp3Test.java
@@ -7,6 +7,11 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import test.org.springdoc.ui.AbstractSpringDocTest;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class SpringDocApp3Test extends AbstractSpringDocTest {
 
     @Test
@@ -18,5 +23,14 @@ public class SpringDocApp3Test extends AbstractSpringDocTest {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/index.html"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.header().string(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE));
+    }
+
+    @Test
+    public void swagger_config_for_no_groups() throws Exception {
+        mockMvc.perform(get("/v3/api-docs/swagger-config"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("configUrl", equalTo("/v3/api-docs/swagger-config")))
+                .andExpect(jsonPath("url", equalTo("/v3/api-docs")))
+                .andExpect(jsonPath("urls").doesNotExist());
     }
 }
