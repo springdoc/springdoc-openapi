6d49fdab4c7d4f2d988765253e8282d7f73813e0
springdoc <springdoc99@gmail.com>
2019-12-25 00:38:50 +0100

    code cleanup

--- springdoc-openapi-webflux-ui/src/main/java/org/springdoc/ui/SwaggerWelcome.java
+++ springdoc-openapi-webflux-ui/src/main/java/org/springdoc/ui/SwaggerWelcome.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.reactive.function.server.RouterFunction;
