4156143f36277baf040ebd5ed6394a5e0533dab6
bnasslahsen <badr.nasslashen@gmail.com>
2020-01-01 22:26:35 +0100

    project cleanup

--- springdoc-openapi-data-rest/src/main/java/org/springdoc/core/converters/PageableAsQueryParam.java
+++ springdoc-openapi-data-rest/src/main/java/org/springdoc/core/converters/PageableAsQueryParam.java
@@ -1,7 +1,6 @@
 package org.springdoc.core.converters;
 
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -14,21 +13,19 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
-@Parameters({
-        @Parameter(in = ParameterIn.QUERY
-                , description = "Zero-based page index (0..N)"
-                , name = "page"
-                , content = @Content(schema = @Schema(type = "integer", defaultValue = "0"))),
-        @Parameter(in = ParameterIn.QUERY
-                , description = "The size of the page to be returned"
-                , name = "size"
-                , content = @Content(schema = @Schema(type = "integer", defaultValue = "20"))),
-        @Parameter(in = ParameterIn.QUERY
-                , description = "Sorting criteria in the format: property(,asc|desc). "
-                + "Default sort order is ascending. " + "Multiple sort criteria are supported."
-                , name = "sort"
-                , content = @Content(array = @ArraySchema(schema = @Schema(type = "string"))))
-})
+@Parameter(in = ParameterIn.QUERY
+        , description = "Zero-based page index (0..N)"
+        , name = "page"
+        , content = @Content(schema = @Schema(type = "integer", defaultValue = "0")))
+@Parameter(in = ParameterIn.QUERY
+        , description = "The size of the page to be returned"
+        , name = "size"
+        , content = @Content(schema = @Schema(type = "integer", defaultValue = "20")))
+@Parameter(in = ParameterIn.QUERY
+        , description = "Sorting criteria in the format: property(,asc|desc). "
+        + "Default sort order is ascending. " + "Multiple sort criteria are supported."
+        , name = "sort"
+        , content = @Content(array = @ArraySchema(schema = @Schema(type = "string"))))
 public @interface PageableAsQueryParam {
 
 }
