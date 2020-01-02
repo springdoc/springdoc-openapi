addeae8c9d26f8b0d3585c1c77a8d576665be9de
m-kay <michi.kreis@gmail.com>
2020-01-02 15:52:52 +0100

    fix typo, add protocol-relative-baseurl to springdoc config propeties

--- springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app71/SpringDocApp71Test.java
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app71/SpringDocApp71Test.java
@@ -3,6 +3,6 @@ package test.org.springdoc.api.app71;
 import org.springframework.test.context.TestPropertySource;
 import test.org.springdoc.api.AbstractSpringDocTest;
 
-@TestPropertySource(properties = "springdoc.protocol-relativ-baseurl=true")
+@TestPropertySource(properties = "springdoc.protocol-relative-baseurl=true")
 public class SpringDocApp71Test extends AbstractSpringDocTest {
 }
