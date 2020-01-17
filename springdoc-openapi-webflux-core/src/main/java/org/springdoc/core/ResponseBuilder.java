f93a1fd7cbacdc6350d6f8fa5c28553c4aca5e03
bnasslahsen <badr.nasslashen@gmail.com>
2020-01-17 20:25:19 +0100

    Improve support of HttpEntity on webflux. Fixes #280

--- springdoc-openapi-webflux-core/src/main/java/org/springdoc/core/ResponseBuilder.java
+++ springdoc-openapi-webflux-core/src/main/java/org/springdoc/core/ResponseBuilder.java
@@ -23,9 +23,8 @@ public class ResponseBuilder extends AbstractResponseBuilder {
                                                           JsonView jsonView) {
         Schema schemaN = null;
         if (Mono.class.getName().contentEquals(parameterizedType.getRawType().getTypeName())) {
-            if (parameterizedType.getActualTypeArguments()[0] instanceof ParameterizedType && ResponseEntity.class
-                    .getName().contentEquals(((ParameterizedType) parameterizedType.getActualTypeArguments()[0])
-                            .getRawType().getTypeName())) {
+            if (parameterizedType.getActualTypeArguments()[0] instanceof ParameterizedType && isResponseTypeToIgnore(((ParameterizedType) parameterizedType.getActualTypeArguments()[0])
+                    .getRawType().getTypeName())) {
                 ParameterizedType parameterizedTypeNew = (ParameterizedType) parameterizedType
                         .getActualTypeArguments()[0];
                 schemaN = calculateSchemaParameterizedType(components, parameterizedTypeNew, jsonView);
@@ -33,19 +32,22 @@ public class ResponseBuilder extends AbstractResponseBuilder {
                 schemaN = calculateSchemaParameterizedType(components, parameterizedType, jsonView);
             }
         } else if (Flux.class.getName().contentEquals(parameterizedType.getRawType().getTypeName())) {
-            if (parameterizedType.getActualTypeArguments()[0] instanceof ParameterizedType && ResponseEntity.class
-                    .getName().contentEquals(((ParameterizedType) parameterizedType.getActualTypeArguments()[0])
-                            .getRawType().getTypeName())) {
+            if (parameterizedType.getActualTypeArguments()[0] instanceof ParameterizedType && isResponseTypeToIgnore(((ParameterizedType) parameterizedType.getActualTypeArguments()[0])
+                    .getRawType().getTypeName())) {
                 ParameterizedType parameterizedTypeNew = (ParameterizedType) parameterizedType
                         .getActualTypeArguments()[0];
                 schemaN = calculateFluxSchema(components, parameterizedTypeNew, jsonView);
             } else {
                 schemaN = calculateFluxSchema(components, parameterizedType, jsonView);
             }
-        } else if (ResponseEntity.class.getName().contentEquals(parameterizedType.getRawType().getTypeName()) || HttpEntity.class.getName().contentEquals(parameterizedType.getRawType().getTypeName())) {
+        } else if (isResponseTypeToIgnore(parameterizedType.getRawType().getTypeName())) {
             schemaN = calculateSchemaParameterizedType(components, parameterizedType, jsonView);
         }
         return schemaN;
+    }
+
+    private boolean isResponseTypeToIgnore(String typeName) {
+        return ResponseEntity.class.getName().contentEquals(typeName) || HttpEntity.class.getName().contentEquals(typeName);
     }
 
     private Schema calculateFluxSchema(Components components, ParameterizedType parameterizedType, JsonView jsonView) {
