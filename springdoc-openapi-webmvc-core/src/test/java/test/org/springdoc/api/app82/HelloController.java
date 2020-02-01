2d3183dd29c40b67a245d76ca1f83b0fdb4ef68e
bnasslahsen <badr.nasslashen@gmail.com>
2020-02-01 21:48:32 +0100

    Improve Management of multiple params in requestBody

--- springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app82/HelloController.java
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app82/HelloController.java
@@ -20,7 +20,6 @@ package test.org.springdoc.api.app82;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
