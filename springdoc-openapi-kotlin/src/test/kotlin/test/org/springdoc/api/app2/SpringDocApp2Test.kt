bfe502706d5aad515ec07f84cbac71d34f5bac5b
petar.tahchiev <paranoiabla@gmail.com>
2020-02-21 12:45:08 +0200

    Fix tests

--- springdoc-openapi-kotlin/src/test/kotlin/test/org/springdoc/api/app2/SpringDocApp2Test.kt
+++ springdoc-openapi-kotlin/src/test/kotlin/test/org/springdoc/api/app2/SpringDocApp2Test.kt
@@ -22,7 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
 import test.org.springdoc.api.AbstractKotlinSpringDocTest
 
-@ComponentScan(basePackages = ["org.springdoc", "test.org.springdoc.api.app2"])
+@ComponentScan(basePackages = ["org.springdoc.core", "test.org.springdoc.api.app2"])
 class SpringDocApp2Test : AbstractKotlinSpringDocTest() {
 
 	@SpringBootApplication
