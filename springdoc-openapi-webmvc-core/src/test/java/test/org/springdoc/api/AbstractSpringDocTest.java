95a5e642b7e232393479a0ed30dc72ed0fc3dd90
petar.tahchiev <paranoiabla@gmail.com>
2020-02-25 21:50:03 +0200

    Add a test-case for builder customizer

--- springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/AbstractSpringDocTest.java
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/AbstractSpringDocTest.java
@@ -47,32 +47,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public abstract class AbstractSpringDocTest {
 
-	protected static final Logger LOGGER = LoggerFactory.getLogger(AbstractSpringDocTest.class);
+    protected static final Logger LOGGER = LoggerFactory.getLogger(AbstractSpringDocTest.class);
 
-	public static String className;
+    public static String className;
 
-	@Autowired
-	protected MockMvc mockMvc;
+    @Autowired
+    protected MockMvc mockMvc;
 
-	public static String getContent(String fileName) throws Exception {
-		try {
-			Path path = Paths.get(FileUtils.class.getClassLoader().getResource(fileName).toURI());
-			byte[] fileBytes = Files.readAllBytes(path);
-			return new String(fileBytes, StandardCharsets.UTF_8);
-		}
-		catch (Exception e) {
-			throw new RuntimeException("Failed to read file: " + fileName, e);
-		}
-	}
+    public static String getContent(String fileName) throws Exception {
+        try {
+            Path path = Paths.get(FileUtils.class.getClassLoader().getResource(fileName).toURI());
+            byte[] fileBytes = Files.readAllBytes(path);
+            return new String(fileBytes, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to read file: " + fileName, e);
+        }
+    }
 
-	@Test
-	public void testApp() throws Exception {
-		className = getClass().getSimpleName();
-		String testNumber = className.replaceAll("[^0-9]", "");
-		MvcResult mockMvcResult = mockMvc.perform(get(Constants.DEFAULT_API_DOCS_URL)).andExpect(status().isOk())
-				.andExpect(jsonPath("$.openapi", is("3.0.1"))).andReturn();
-		String result = mockMvcResult.getResponse().getContentAsString();
-		String expected = getContent("results/app" + testNumber + ".json");
-		assertEquals(expected, result, true);
-	}
+    @Test
+    public void testApp() throws Exception {
+        className = getClass().getSimpleName();
+        String testNumber = className.replaceAll("[^0-9]", "");
+        MvcResult mockMvcResult = mockMvc.perform(get(Constants.DEFAULT_API_DOCS_URL)).andExpect(status().isOk()).andExpect(jsonPath("$.openapi", is("3.0.1")))
+                                         .andReturn();
+        String result = mockMvcResult.getResponse().getContentAsString();
+        String expected = getContent("results/app" + testNumber + ".json");
+        assertEquals(expected, result, true);
+    }
 }
---
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app92/SpringDocApp92Test.java
@@ -0,0 +1,123 @@
+/*
+ *
+ *  * Copyright 2019-2020 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+package test.org.springdoc.api.app92;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springdoc.api.ActuatorProvider;
+import org.springdoc.api.OpenApiResource;
+import org.springdoc.core.AbstractRequestBuilder;
+import org.springdoc.core.GenericResponseBuilder;
+import org.springdoc.core.OpenAPIBuilder;
+import org.springdoc.core.OperationBuilder;
+import org.springdoc.core.SpringDocConfigProperties;
+import org.springdoc.core.customizers.OpenApiBuilderCustomiser;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import test.org.springdoc.api.AbstractSpringDocTest;
+import test.org.springdoc.api.app91.Greeting;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springdoc.core.Constants.DEFAULT_GROUP_NAME;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@TestPropertySource(properties = "springdoc.default-produces-media-type=application/json")
+public class SpringDocApp92Test extends AbstractSpringDocTest {
+
+    @SpringBootApplication
+    static class SpringDocTestApp implements ApplicationContextAware {
+
+        private ApplicationContext applicationContext;
+
+        @Bean
+        public GreetingController greetingController() {
+            return new GreetingController();
+        }
+
+        @Bean
+        public OpenApiBuilderCustomiser customOpenAPI() {
+            return openApiBuilder -> openApiBuilder.addMappings(Collections.singletonMap("greetingController", new GreetingController()));
+        }
+
+        @Bean
+        public RequestMappingHandlerMapping defaultTestHandlerMapping(GreetingController greetingController) throws NoSuchMethodException {
+            RequestMappingHandlerMapping result = new RequestMappingHandlerMapping();
+            RequestMappingInfo requestMappingInfo =
+                            RequestMappingInfo.paths("/test").methods(RequestMethod.GET).produces(MediaType.APPLICATION_JSON_VALUE).build();
+
+            result.setApplicationContext(this.applicationContext);
+            result.registerMapping(requestMappingInfo, "greetingController", GreetingController.class.getDeclaredMethod("sayHello2"));
+            //result.handlerme
+            return result;
+        }
+
+        @Bean(name = "mvcOpenApiResource")
+        public OpenApiResource openApiResource(OpenAPIBuilder openAPIBuilder, AbstractRequestBuilder requestBuilder, GenericResponseBuilder responseBuilder,
+                                               OperationBuilder operationParser,
+                                               @Qualifier("defaultTestHandlerMapping") RequestMappingHandlerMapping requestMappingHandlerMapping,
+                                               Optional<ActuatorProvider> servletContextProvider, SpringDocConfigProperties springDocConfigProperties,
+                                               Optional<List<OpenApiCustomiser>> openApiCustomisers) {
+            return new OpenApiResource(DEFAULT_GROUP_NAME, openAPIBuilder, requestBuilder, responseBuilder, operationParser, requestMappingHandlerMapping,
+                                       servletContextProvider, openApiCustomisers, springDocConfigProperties);
+        }
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            this.applicationContext = applicationContext;
+        }
+    }
+
+    @ResponseBody
+    @Tag(name = "Demo", description = "The Demo API")
+    public static class GreetingController {
+
+        @GetMapping(produces = APPLICATION_JSON_VALUE)
+        @Operation(summary = "This API will return a random greeting.")
+        public ResponseEntity<Greeting> sayHello() {
+            return ResponseEntity.ok(new Greeting(RandomStringUtils.randomAlphanumeric(10)));
+        }
+
+        @GetMapping("/test")
+        @ApiResponses(value = { @ApiResponse(responseCode = "201", description = "item created"),
+                        @ApiResponse(responseCode = "400", description = "invalid input, object invalid"),
+                        @ApiResponse(responseCode = "409", description = "an existing item already exists") })
+        public ResponseEntity<Greeting> sayHello2() {
+            return ResponseEntity.ok(new Greeting(RandomStringUtils.randomAlphanumeric(10)));
+        }
+
+    }
+}
