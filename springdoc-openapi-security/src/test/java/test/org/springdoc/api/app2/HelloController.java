ce105c952915a5d4d9917dc51010c306a199acca
João Dias <joao.dias@mercedes-benz.io>
2020-03-22 18:37:40 +0000

    Subtypes also ignored from Controller class

--- springdoc-openapi-security/src/test/java/test/org/springdoc/api/app2/HelloController.java
+++ springdoc-openapi-security/src/test/java/test/org/springdoc/api/app2/HelloController.java
@@ -18,6 +18,7 @@
 
 package test.org.springdoc.api.app2;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,10 +26,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-
-	@GetMapping
-	public Object doPost(@RequestBody String req, org.springframework.security.core.Authentication auth) {
+	@GetMapping("oneParameterAndAuthentication")
+	public Object oneParameterAndAuthentication(@RequestBody String req, Authentication authentication) {
 		return null;
 	}
 
+	@GetMapping("noParametersAndAuthentication")
+	public Object noParametersAndAuthentication(Authentication authentication) {
+		return null;
+	}
+
+	@GetMapping("oneParameterAndUser")
+	public Object oneParameterAndUser(@RequestBody String req, User user) {
+		return null;
+	}
+
+	@GetMapping("noParametersAndUser")
+	public Object noParametersAndUser(User user) {
+		return null;
+	}
 }
