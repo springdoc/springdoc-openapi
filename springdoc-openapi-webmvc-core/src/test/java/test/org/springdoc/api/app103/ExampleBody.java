f9f4d1614e2e24f594051224a036dd75e85ce09b
Patryk Pacewicz <patrykpacewicz@gmail.com>
2020-04-02 17:30:38 +0200

    Fixing encoding section in multipart request Fixes #531

---
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app103/ExampleBody.java
@@ -0,0 +1,23 @@
+package test.org.springdoc.api.app103;
+
+public class ExampleBody {
+    private String stringParam;
+
+    private int intParam;
+
+    public String getStringParam() {
+        return stringParam;
+    }
+
+    public void setStringParam(String stringParam) {
+        this.stringParam = stringParam;
+    }
+
+    public int getIntParam() {
+        return intParam;
+    }
+
+    public void setIntParam(int intParam) {
+        this.intParam = intParam;
+    }
+}
