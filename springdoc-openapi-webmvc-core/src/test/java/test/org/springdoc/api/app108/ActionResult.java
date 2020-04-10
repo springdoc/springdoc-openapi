65c0cb0723713ea1bf53828a8843a07a57631729
bnasslahsen <badr.nasslashen@gmail.com>
2020-04-10 21:32:50 +0200

    The response scheme is erased with the generic void type. Fixes #555.

---
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app108/ActionResult.java
@@ -0,0 +1,59 @@
+package test.org.springdoc.api.app108;
+
+public class ActionResult<T> {
+
+	protected T value;
+	protected boolean success;
+	protected String errorCode;
+	protected String message;
+	protected Object errorValue;
+	protected String targetUrl;
+
+	public T getValue() {
+		return value;
+	}
+
+	public void setValue(T value) {
+		this.value = value;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public String getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Object getErrorValue() {
+		return errorValue;
+	}
+
+	public void setErrorValue(Object errorValue) {
+		this.errorValue = errorValue;
+	}
+
+	public String getTargetUrl() {
+		return targetUrl;
+	}
+
+	public void setTargetUrl(String targetUrl) {
+		this.targetUrl = targetUrl;
+	}
+}
---
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app108/SpringDocApp108Test.java
@@ -0,0 +1,10 @@
+package test.org.springdoc.api.app108;
+
+import test.org.springdoc.api.AbstractSpringDocTest;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class SpringDocApp108Test extends AbstractSpringDocTest {
+	@SpringBootApplication
+	static class SpringDocTestApp {}
+}
