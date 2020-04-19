6d003aed73a7a3dc274b5b55e6254f7b88493fda
bnasslahsen <badr.nasslashen@gmail.com>
2020-04-19 12:26:39 +0200

    Crash on startup with 1.3.3 for kotlin app not using reactor. Fixes #583

---
+++ springdoc-openapi-kotlin/src/main/java/org/springdoc/kotlin/SpringDocKotlinxConfiguration.java
@@ -0,0 +1,21 @@
+package org.springdoc.kotlin;
+
+import kotlinx.coroutines.flow.Flow;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+import static org.springdoc.core.Constants.SPRINGDOC_ENABLED;
+import static org.springdoc.core.SpringDocUtils.getConfig;
+
+@ConditionalOnClass(Flow.class)
+@Configuration
+@ConditionalOnProperty(name = SPRINGDOC_ENABLED, matchIfMissing = true)
+public class SpringDocKotlinxConfiguration {
+
+	static {
+		getConfig().addFluxWrapperToIgnore(Flow.class);
+	}
+
+}
