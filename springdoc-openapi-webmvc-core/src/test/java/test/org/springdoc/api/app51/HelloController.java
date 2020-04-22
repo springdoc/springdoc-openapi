48d9a8ab043ee56c10d5ba66b3b678a81b845e21
bnasslahsen <badr.nasslashen@gmail.com>
2020-04-22 17:32:30 +0200

    Request Body for Maps not available in Swagger-UI. Fixes #597

--- springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app51/HelloController.java
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app51/HelloController.java
@@ -18,6 +18,7 @@
 
 package test.org.springdoc.api.app51;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -25,8 +26,11 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -63,4 +67,8 @@ public class HelloController {
 		return null;
 	}
 
+	@PostMapping
+	public ResponseEntity<HashMap<String, Object>> hello(@RequestBody HashMap<String, Object> map) {
+		return ResponseEntity.ok(map);
+	}
 }
