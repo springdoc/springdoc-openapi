17cb80ab3caaf73108cb243a911431c111276449
bnasslahsen <badr.nasslashen@gmail.com>
2020-04-23 17:04:05 +0200

    assignableTypes attribute in @ControllerAdvice. Fixes #588.

---
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app110/Person.java
@@ -0,0 +1,88 @@
+package sample;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+
+public class Person {
+	private long id;
+	private String firstName;
+	@NotNull
+	@NotBlank
+	@Size(max = 10)
+	private String lastName;
+	@Pattern(regexp = ".+@.+\\..+", message = "Please provide a valid email address")
+	private String email;
+	@Email()
+	private String email1;
+	@Min(18)
+	@Max(30)
+	private int age;
+	@CreditCardNumber
+	private String creditCardNumber;
+
+	public String getCreditCardNumber() {
+		return creditCardNumber;
+	}
+
+	public void setCreditCardNumber(String creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getEmail1() {
+		return email1;
+	}
+
+	public void setEmail1(String email1) {
+		this.email1 = email1;
+	}
+
+	@Size(min = 2)
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
---
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app110/PersonController.java
@@ -0,0 +1,50 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Validated
+public class PersonController {
+	private Random ran = new Random(); 
+	@RequestMapping(path = "/person", method = RequestMethod.POST)
+	public Person person(@Valid @RequestBody Person person) {
+		 
+	     int nxt = ran.nextInt(10); 
+	     if(nxt>=5)
+	     {
+	    	 throw new RuntimeException("Breaking logic");
+	     }
+		return person;
+	}
+	@RequestMapping(path = "/personByLastName", method = RequestMethod.GET)
+	public List<Person> findByLastName(@RequestParam(name = "lastName", required = true)@NotNull
+			@NotBlank
+			@Size(max = 10)String lastName){
+		List<Person> hardCoded= new ArrayList<>();
+		Person person= new Person();
+		person.setAge(20);
+		person.setCreditCardNumber("4111111111111111");
+		person.setEmail("abc@abc.com");
+		person.setEmail1("abc1@abc.com");
+		person.setFirstName("Somefirstname");
+		person.setLastName(lastName);
+		person.setId(1);
+		hardCoded.add(person);
+		return hardCoded;
+		
+	}
+}
