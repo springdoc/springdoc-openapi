ca4927361f4b6c1524b956d1e5770a61b7bed7a3
bnasslahsen <badr.nasslashen@gmail.com>
2020-04-28 15:35:56 +0200

    Missing extension to schema property. Fixes #622.

--- springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app114/SpringDocApp114Test.java
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app114/SpringDocApp114Test.java
@@ -1,5 +1,8 @@
 package test.org.springdoc.api.app114;
 
+import javax.money.MonetaryAmount;
+
+import org.springdoc.core.SpringDocUtils;
 import test.org.springdoc.api.AbstractSpringDocTest;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,4 +12,8 @@ public class SpringDocApp114Test extends AbstractSpringDocTest {
 
 	@SpringBootApplication
 	static class SpringDocTestApp {}
+
+	static {
+		SpringDocUtils.getConfig().replaceWithClass(MonetaryAmount.class, org.springdoc.core.converters.MonetaryAmount.class);
+	}
 }
