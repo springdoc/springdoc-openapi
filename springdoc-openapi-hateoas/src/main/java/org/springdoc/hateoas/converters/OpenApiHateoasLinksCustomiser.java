0afe9e1d5d94531907fb8c7049a6412d51c1132c
bnasslahsen <badr.nasslashen@gmail.com>
2020-07-01 21:31:30 +0200

    improve compatibility with java 11

---
+++ springdoc-openapi-hateoas/src/main/java/org/springdoc/hateoas/converters/OpenApiHateoasLinksCustomiser.java
@@ -0,0 +1,41 @@
+package org.springdoc.hateoas.converters;
+
+import io.swagger.v3.core.converter.AnnotatedType;
+import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.core.converter.ResolvedSchema;
+import io.swagger.v3.core.filter.SpecFilter;
+import io.swagger.v3.core.util.AnnotationsUtils;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.MapSchema;
+import io.swagger.v3.oas.models.media.ObjectSchema;
+import io.swagger.v3.oas.models.media.StringSchema;
+import org.springdoc.core.SpringDocConfigProperties;
+import org.springdoc.core.customizers.OpenApiCustomiser;
+
+import org.springframework.hateoas.Link;
+
+/**
+ * The type Open api hateoas links customiser.
+ * @author bnasslahsen
+ */
+public class OpenApiHateoasLinksCustomiser extends SpecFilter implements OpenApiCustomiser {
+
+	private final SpringDocConfigProperties springDocConfigProperties;
+
+	public OpenApiHateoasLinksCustomiser(SpringDocConfigProperties springDocConfigProperties) {
+		this.springDocConfigProperties = springDocConfigProperties;
+	}
+
+	@Override
+	public void customise(OpenAPI openApi) {
+		ResolvedSchema resolvedLinkSchema = ModelConverters.getInstance()
+				.resolveAsResolvedSchema(new AnnotatedType(Link.class));
+		openApi
+				.schema("Link", resolvedLinkSchema.schema)
+				.schema("Links", new MapSchema()
+						.additionalProperties(new StringSchema())
+						.additionalProperties(new ObjectSchema().$ref(AnnotationsUtils.COMPONENTS_REF + "Link")));
+		if (springDocConfigProperties.isRemoveBrokenReferenceDefinitions())
+			this.removeBrokenReferenceDefinitions(openApi);
+	}
+}
