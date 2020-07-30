a59bf6934bec98d95b187c6aac4d06ad79b784ef
bnasslahsen <badr.nasslashen@gmail.com>
2020-07-30 17:40:42 +0200

    Generate api-docs for Custom Actuator Endpoint. Fixes #786

--- springdoc-openapi-common/src/main/java/org/springdoc/core/customizers/ActuatorOpenApiCustomiser.java
+++ springdoc-openapi-common/src/main/java/org/springdoc/core/customizers/ActuatorOpenApiCustomiser.java
@@ -1,12 +1,17 @@
 package org.springdoc.core.customizers;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.oas.models.parameters.PathParameter;
+
+import org.springframework.util.CollectionUtils;
 
 /**
  * The type Actuator open api customiser.
@@ -26,7 +31,14 @@ public class ActuatorOpenApiCustomiser implements OpenApiCustomiser {
 					while (matcher.find()) {
 						String pathParam = matcher.group(1);
 						PathItem pathItem = stringPathItemEntry.getValue();
-						pathItem.readOperations().forEach(operation -> operation.addParametersItem(new PathParameter().name(pathParam).schema(new StringSchema())));
+						pathItem.readOperations().forEach(operation -> {
+							List<Parameter> existingParameters = operation.getParameters();
+							Optional<Parameter> existingParam = Optional.empty();
+							if (!CollectionUtils.isEmpty(existingParameters))
+								existingParam = existingParameters.stream().filter(p -> pathParam.equals(p.getName())).findAny();
+							if (!existingParam.isPresent())
+								operation.addParametersItem(new PathParameter().name(pathParam).schema(new StringSchema()));
+						});
 					}
 				});
 	}
