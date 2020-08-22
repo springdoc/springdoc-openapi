e3c647142e58d926a9a6fc3492aeced9dea75a98
bnasslahsen <badr.nasslashen@gmail.com>
2020-08-22 11:17:30 +0200

    upgrade swagger-ui to 3.32.3.

--- springdoc-openapi-webflux-core/src/test/java/test/org/springdoc/api/app79/BaseController.java
+++ springdoc-openapi-webflux-core/src/test/java/test/org/springdoc/api/app79/BaseController.java
@@ -2,20 +2,17 @@ package test.org.springdoc.api.app79;
 
 import java.util.List;
 
-import io.swagger.v3.oas.annotations.Operation;
 import reactor.core.publisher.Mono;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public abstract class BaseController<TClientModel extends BaseClientModel> {
-	@Operation
 	@GetMapping("/test1")
 	Mono<ResponseEntity<TClientModel>> get1() {
 		return null;
 	}
 
-	@Operation
 	@GetMapping("/test2")
 	Mono<ResponseEntity<List<TClientModel>>> get2() {
 		return null;
