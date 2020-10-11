4dd6c9bc850bdceb958e3ca35ab7696d7035405b
bnasslahsen <badr.nasslashen@gmail.com>
2020-10-11 18:13:31 +0200

    Generate empty `scopes` object. Fixes #893.

---
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app137/OpenApiConfig.java
@@ -0,0 +1,34 @@
+/*
+ *
+ *  * Copyright 2019-2020 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package test.org.springdoc.api.app137;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@OpenAPIDefinition(info = @Info(title = "My App",
+		description = "Some long and useful description", version = "v1"))
+@SecurityScheme(name = "security_auth", type = SecuritySchemeType.OAUTH2,
+		flows = @OAuthFlows(authorizationCode = @OAuthFlow(
+				authorizationUrl = "http://authorization.url"
+				, tokenUrl = "http://token.url", scopes = { })))
+public class OpenApiConfig {}
