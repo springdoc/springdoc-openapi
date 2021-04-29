0942572afd482caab507536deb86e543de2533d8
Jared Gentner <jgentner@jobcase.com>
2021-04-29 14:35:27 -0400

    add test to demonstrate

---
+++ springdoc-openapi-webmvc-core/src/test/java/test/org/springdoc/api/app157/Foo.java
@@ -0,0 +1,12 @@
+package test.org.springdoc.api.app157;
+
+/**
+ * A class with a String in it
+ */
+public class Foo {
+  private String child;
+
+  public String getChild() {
+    return this.child;
+  }
+}
