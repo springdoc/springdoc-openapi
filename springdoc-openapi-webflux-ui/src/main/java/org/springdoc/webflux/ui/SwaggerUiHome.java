58272e70b81cc8ddc2dc191f9a57443db97b57b3
bnasslah <badr.nasslahsen@gmail.com>
2021-11-17 21:26:58 +0100

    update SwaggerUiHome for webflux

--- springdoc-openapi-webflux-ui/src/main/java/org/springdoc/webflux/ui/SwaggerUiHome.java
+++ springdoc-openapi-webflux-ui/src/main/java/org/springdoc/webflux/ui/SwaggerUiHome.java
@@ -21,11 +21,14 @@
 package org.springdoc.webflux.ui;
 
 import java.net.URI;
+import java.util.Optional;
 
 import io.swagger.v3.oas.annotations.Operation;
+import org.apache.commons.lang3.StringUtils;
 import reactor.core.publisher.Mono;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Controller;
@@ -37,17 +40,41 @@ import static org.springframework.util.AntPathMatcher.DEFAULT_PATH_SEPARATOR;
 
 /**
  * Home redirection to swagger api documentation
+ * @author bnasslahsen
  */
 @Controller
 public class SwaggerUiHome {
 
+	/**
+	 * The Swagger ui path.
+	 */
 	@Value(SWAGGER_UI_PATH)
 	private String swaggerUiPath;
 
+	/**
+	 * The Base path.
+	 */
+	private String basePath = StringUtils.EMPTY;
+
+	/**
+	 * Instantiates a new Swagger ui home.
+	 *
+	 * @param optionalWebFluxProperties the optional web flux properties
+	 */
+	public SwaggerUiHome(Optional<WebFluxProperties> optionalWebFluxProperties) {
+		optionalWebFluxProperties.ifPresent(webFluxProperties -> this.basePath = webFluxProperties.getBasePath());
+	}
+
+	/**
+	 * Index mono.
+	 *
+	 * @param response the response
+	 * @return the mono
+	 */
 	@GetMapping(DEFAULT_PATH_SEPARATOR)
 	@Operation(hidden = true)
 	public Mono<Void> index(ServerHttpResponse response) {
-		UriComponentsBuilder uriBuilder =  UriComponentsBuilder.fromUriString(swaggerUiPath);
+		UriComponentsBuilder uriBuilder =  UriComponentsBuilder.fromUriString(this.basePath + swaggerUiPath);
 		response.setStatusCode(HttpStatus.FOUND);
 		response.getHeaders().setLocation(URI.create(uriBuilder.build().encode().toString()));
 		return response.setComplete();
