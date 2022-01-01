60de6d4cc4117ce402064529496f88d82c49c2e9
bnasslah <badr.nasslahsen@gmail.com>
2022-01-01 20:28:18 +0100

    code review

--- springdoc-openapi-ui/src/main/java/org/springdoc/webmvc/ui/SwaggerWebMvcConfigurer.java
+++ springdoc-openapi-ui/src/main/java/org/springdoc/webmvc/ui/SwaggerWebMvcConfigurer.java
@@ -20,12 +20,29 @@
 
 package org.springdoc.webmvc.ui;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springdoc.core.SwaggerUiConfigParameters;
 import org.springdoc.core.providers.ActuatorProvider;
 
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.validation.Validator;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springdoc.core.Constants.CLASSPATH_RESOURCE_LOCATION;
@@ -37,8 +54,7 @@ import static org.springframework.util.AntPathMatcher.DEFAULT_PATH_SEPARATOR;
  * The type Swagger web mvc configurer.
  * @author bnasslahsen
  */
-@SuppressWarnings("deprecation")
-public class SwaggerWebMvcConfigurer implements WebMvcConfigurer { // NOSONAR
+public class SwaggerWebMvcConfigurer implements WebMvcConfigurer {
 
 	/**
 	 * The Swagger path.
@@ -84,4 +100,93 @@ public class SwaggerWebMvcConfigurer implements WebMvcConfigurer { // NOSONAR
 				.addTransformer(swaggerIndexTransformer);
 	}
 
+	@Override
+	public void configurePathMatch(PathMatchConfigurer configurer) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void addReturnValueHandlers(List<HandlerMethodReturnValueHandler> returnValueHandlers) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+	}
+
+	@Override
+	@Nullable
+	public Validator getValidator() {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+		return null;
+	}
+
+	@Override
+	@Nullable
+	public MessageCodesResolver getMessageCodesResolver() {
+		// This implementation is empty to keep compatibility with spring 4 applications.
+		return null;
+	}
 }
