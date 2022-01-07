c8cace23d49ded303e3825a1ec3599ac1fb459db
predicate@bonzo <noreply@example.com>
2022-01-07 14:53:42 +0100

    add test

---
+++ springdoc-openapi-security/src/test/java/test/org/springdoc/api/app8/security/WebConfig.java
@@ -0,0 +1,17 @@
+package test.org.springdoc.api.app8.security;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Order(200)
+public class WebConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginProcessingUrl("/api/login");
+    }
+}
