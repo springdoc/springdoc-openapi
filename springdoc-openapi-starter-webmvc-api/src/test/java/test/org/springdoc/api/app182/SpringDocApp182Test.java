82517694fd9db70038d957ae5128c2a0e1c0a021
bnasslah <badr.nasslahsen@gmail.com>
2022-02-27 14:08:58 +0100

    Changes report: Merge pull request #1500 from eshishkin/master  Pick up exception handler in case there is no controller advice at alll, fixes 1498

---
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/app182/SpringDocApp182Test.java
@@ -0,0 +1,14 @@
+package test.org.springdoc.api.app182;
+
+import test.org.springdoc.api.AbstractSpringDocTest;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bnasslahsen
+ */
+public class SpringDocApp182Test extends AbstractSpringDocTest {
+
+	@SpringBootApplication
+	static class SpringDocTestApp {}
+}
