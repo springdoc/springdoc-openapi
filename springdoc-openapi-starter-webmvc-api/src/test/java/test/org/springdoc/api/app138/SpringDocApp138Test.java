417a8e78b5c99f0a4adac08ee0244463c7c34eec
bnasslah <badr.nasslahsen@gmail.com>
2022-03-05 16:17:56 +0100

    Changes report: Fix tests

--- springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/app138/SpringDocApp138Test.java
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/app138/SpringDocApp138Test.java
@@ -22,12 +22,18 @@
  */
 package test.org.springdoc.api.app138;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.core.util.Json;
+import io.swagger.v3.core.util.ObjectMapperFactory;
 import io.swagger.v3.oas.models.OpenAPI;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springdoc.core.utils.Constants;
 import test.org.springdoc.api.AbstractSpringDocTest;
@@ -75,5 +81,19 @@ public class SpringDocApp138Test extends AbstractSpringDocTest {
 		String result = mockMvcResult.getResponse().getContentAsString();
 		String expected = getContent("results/app138.json");
 		Assertions.assertEquals(expected, result);
+	}
+
+
+	@BeforeEach
+	void init() throws IllegalAccessException {
+		Field conField = FieldUtils.getDeclaredField(Json.class, "mapper", true);
+		ObjectMapper mapper = SpringDocObjectMapperFactory.createJson();
+		conField.set(mapper, mapper);
+	}
+
+	private static class SpringDocObjectMapperFactory extends ObjectMapperFactory {
+		protected static ObjectMapper createJson() {
+			return ObjectMapperFactory.createJson();
+		}
 	}
 }
