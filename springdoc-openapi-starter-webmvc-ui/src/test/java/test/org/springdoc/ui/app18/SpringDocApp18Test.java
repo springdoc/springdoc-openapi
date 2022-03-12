14051090dc534749da697c879d59a880d8e35255
bnasslah <badr.nasslahsen@gmail.com>
2022-03-12 11:32:29 +0100

    Changes report: Setting displayOperationId to false does not work. Fixes #1530.

--- springdoc-openapi-starter-webmvc-ui/src/test/java/test/org/springdoc/ui/app18/SpringDocApp18Test.java
+++ springdoc-openapi-starter-webmvc-ui/src/test/java/test/org/springdoc/ui/app18/SpringDocApp18Test.java
@@ -29,18 +29,22 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@TestPropertySource(properties = "springdoc.swagger-ui.try-it-out-enabled=true" )
+@TestPropertySource(properties = {
+		"springdoc.swagger-ui.try-it-out-enabled=true",
+		"springdoc.swagger-ui.display-operation-id=true"
+} )
 public class SpringDocApp18Test extends AbstractSpringDocTest {
 
 	@Test
 	public void testTryItOutEnabled() throws Exception {
 		mockMvc.perform(get("/v3/api-docs/swagger-config"))
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("tryItOutEnabled", equalTo("true")));
+				.andExpect(jsonPath("displayOperationId", equalTo(true)))
+				.andExpect(jsonPath("tryItOutEnabled", equalTo(true)));
 	}
 
-    @SpringBootApplication
-    static class SpringDocTestApp {
-    }
+	@SpringBootApplication
+	static class SpringDocTestApp {
+	}
 
 }
