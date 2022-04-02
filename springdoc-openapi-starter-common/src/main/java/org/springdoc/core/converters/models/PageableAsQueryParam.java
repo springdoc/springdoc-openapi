1ec43c9fd7fa0568499a07215d193cda84246cc1
bnasslah <badr.nasslahsen@gmail.com>
2022-04-02 16:47:47 +0200

    Changes report: Typo in Pageable sort description. Fixes #1586.

--- springdoc-openapi-starter-common/src/main/java/org/springdoc/core/converters/models/PageableAsQueryParam.java
+++ springdoc-openapi-starter-common/src/main/java/org/springdoc/core/converters/models/PageableAsQueryParam.java
@@ -47,7 +47,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 		, name = "size"
 		, schema = @Schema(type = "integer", defaultValue = "20"))
 @Parameter(in = ParameterIn.QUERY
-		, description = "Sorting criteria in the format: property(,asc|desc). "
+		, description = "Sorting criteria in the format: property,(asc|desc). "
 		+ "Default sort order is ascending. " + "Multiple sort criteria are supported."
 		, name = "sort"
 		, array = @ArraySchema(schema = @Schema(type = "string")))
