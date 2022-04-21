c79b20d02fcf60bb75f3fdf8a216cebeaf201f38
bnasslah <badr.nasslahsen@gmail.com>
2022-04-21 19:29:20 +0200

    Changes report: Unexpected path behaviour when using management port (actuator) #1603

--- springdoc-openapi-starter-webflux-ui/src/test/java/test/org/springdoc/ui/app14/SpringDocApp14Test.java
+++ springdoc-openapi-starter-webflux-ui/src/test/java/test/org/springdoc/ui/app14/SpringDocApp14Test.java
@@ -53,14 +53,14 @@ class SpringDocApp14Test extends AbstractSpringDocActuatorTest {
 
 	@Test
 	public void testIndexActuator() {
-		HttpStatus httpStatusMono = webClient.get().uri("/application/swaggerui")
+		HttpStatus httpStatusMono = webClient.get().uri("/application/swagger-ui")
 				.exchangeToMono( clientResponse -> Mono.just(clientResponse.statusCode())).block();
 		assertThat(httpStatusMono).isEqualTo(HttpStatus.FOUND);
 	}
 
 	@Test
 	public void testIndexSwaggerConfig() throws Exception {
-		String contentAsString = webClient.get().uri("/application/swaggerui/swagger-config").retrieve()
+		String contentAsString = webClient.get().uri("/application/swagger-ui/swagger-config").retrieve()
 				.bodyToMono(String.class).block();
 		String expected = getContent("results/app14-1.json");
 		assertEquals(expected, contentAsString, true);
