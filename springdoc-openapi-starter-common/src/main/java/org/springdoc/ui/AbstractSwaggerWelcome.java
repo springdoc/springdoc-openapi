64d369f0c05da5a2fe362cd35c852d9f16da91e3
bnasslah <badr.nasslahsen@gmail.com>
2022-05-18 09:46:07 +0200

    Changes report: fix oauth redirection when used in spring-native #1659

--- springdoc-openapi-starter-common/src/main/java/org/springdoc/ui/AbstractSwaggerWelcome.java
+++ springdoc-openapi-starter-common/src/main/java/org/springdoc/ui/AbstractSwaggerWelcome.java
@@ -34,6 +34,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import static org.springdoc.core.utils.Constants.INDEX_PAGE;
+import static org.springdoc.core.utils.Constants.OAUTH_REDIRECT_PAGE;
 import static org.springdoc.core.utils.Constants.SWAGGER_UI_OAUTH_REDIRECT_URL;
 import static org.springdoc.core.utils.Constants.SWAGGER_UI_PREFIX;
 import static org.springdoc.core.utils.Constants.SWAGGER_UI_URL;
@@ -223,7 +224,12 @@ public abstract class AbstractSwaggerWelcome {
 	 * @return the oauth2 redirect url
 	 */
 	protected String getOauth2RedirectUrl() {
-		return StringUtils.defaultIfBlank(swaggerUiConfig.getOauth2RedirectUrl(), SWAGGER_UI_OAUTH_REDIRECT_URL);
+		if (StringUtils.isNotEmpty(swaggerUiConfig.getVersion())) {
+			return StringUtils.defaultIfBlank(swaggerUiConfig.getOauth2RedirectUrl(), SWAGGER_UI_PREFIX + DEFAULT_PATH_SEPARATOR + swaggerUiConfig.getVersion() + OAUTH_REDIRECT_PAGE);
+		}
+		else {
+			return StringUtils.defaultIfBlank(swaggerUiConfig.getOauth2RedirectUrl(), SWAGGER_UI_OAUTH_REDIRECT_URL);
+		}
 	}
 
 	/**
