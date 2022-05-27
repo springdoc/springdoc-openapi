efa6921cec03d482fb4fea04b727d68b8e98b50a
bnasslahsen <badr.nasslashen@gmail.com>
2022-05-27 23:03:43 +0200

    tests review

--- springdoc-openapi-tests/springdoc-openapi-actuator-webflux-tests/src/test/java/test/org/springdoc/api/app148/SpringDocApp148Test.java
+++ springdoc-openapi-tests/springdoc-openapi-actuator-webflux-tests/src/test/java/test/org/springdoc/api/app148/SpringDocApp148Test.java
@@ -37,7 +37,7 @@ import static org.skyscreamer.jsonassert.JSONAssert.assertEquals;
 		properties = { "management.endpoints.web.exposure.include:*",
 				"springdoc.show-actuator=true",
 				"management.endpoints.web.exposure.exclude=functions, shutdown",
-				"management.server.port=9298",
+				"management.server.port=9288",
 				"server.port=6266",
 				"springdoc.use-management-port=true",
 				"management.server.base-path=/test",
