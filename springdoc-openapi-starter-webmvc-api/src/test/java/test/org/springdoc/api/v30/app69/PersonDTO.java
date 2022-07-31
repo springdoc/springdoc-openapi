df356968443bf3fa725b4f4925baca6aab318360
bnasslahsen <badr.nasslashen@gmail.com>
2022-07-31 02:28:42 +0200

    Support Java record for @ParameterObejct annotation. Fixes #1728

--- springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app69/PersonDTO.java
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app69/PersonDTO.java
@@ -24,43 +24,5 @@
 
 package test.org.springdoc.api.v30.app69;
 
-public class PersonDTO {
-	private String email;
-
-	private String firstName;
-
-	private String lastName;
-
-	public PersonDTO() {
-	}
-
-	public PersonDTO(final String email, final String firstName, final String lastName) {
-		this.email = email;
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(final String email) {
-		this.email = email;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(final String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(final String lastName) {
-		this.lastName = lastName;
-	}
+public record PersonDTO (String email,String firstName,String lastName){
 }
