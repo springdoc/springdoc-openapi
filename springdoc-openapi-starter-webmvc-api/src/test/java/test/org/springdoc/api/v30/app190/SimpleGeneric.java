c2cbacb662aee3d7da9b25540fda7c0e1c05da62
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-08-02 23:35:11 +0200

    Changes report: #1771

---
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app190/SimpleGeneric.java
@@ -0,0 +1,15 @@
+package test.org.springdoc.api.v30.app190;
+
+public class SimpleGeneric<T> {
+
+    private String name;
+    private T child;
+
+    public T getChild() {
+        return child;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
