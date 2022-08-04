2c019b3e1feccf9ce42c01b027aa8e658f79ce53
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-08-04 17:50:36 +0200

    Changes report #1778

---
+++ springdoc-openapi-tests/springdoc-openapi-javadoc-tests/src/test/java/test/org/springdoc/api/app166/SimpleInnerClass.java
@@ -0,0 +1,31 @@
+package test.org.springdoc.api.app166;
+
+public class SimpleInnerClass {
+
+    /**
+     * Returns the inner inner class
+     */
+    private SimpleInnerInnerClass innerInnerClass;
+
+    /**
+     * Returns the boolean name
+     */
+    private Boolean name;
+
+    /**
+     * Returns the max number
+     */
+    private Integer maxNumber;
+
+    public Integer getMaxNumber() {
+        return maxNumber;
+    }
+
+    public Boolean getName() {
+        return name;
+    }
+
+    public SimpleInnerInnerClass getInnerInnerClass() {
+        return innerInnerClass;
+    }
+}
---
+++ springdoc-openapi-tests/springdoc-openapi-javadoc-tests/src/test/java/test/org/springdoc/api/app166/SimpleOuterClass.java
@@ -0,0 +1,22 @@
+package test.org.springdoc.api.app166;
+
+public class SimpleOuterClass {
+
+    /**
+     * Returns the name of the outer class
+     */
+    private String name;
+
+    /**
+     * Returns the inner class
+     */
+    private SimpleInnerClass innerClass;
+
+    public String getName() {
+        return name;
+    }
+
+    public SimpleInnerClass getInnerClass() {
+        return innerClass;
+    }
+}
