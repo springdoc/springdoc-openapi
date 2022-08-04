eed76db60613279e34c5c435004177c9d552f524
Rudolf Grauberger <rudolf.grauberger@impect.com>
2022-08-04 09:59:49 +0200

    Fixes NPE for nested parameters with a depth > 1

---
+++ springdoc-openapi-javadoc/src/test/java/test/org/springdoc/api/app166/MyData.java
@@ -0,0 +1,22 @@
+package test.org.springdoc.api.app166;
+
+public class MyData {
+
+    /**
+     * Returns the first name
+     */
+    private String firstName;
+
+    /**
+     * Returns the max number
+     */
+    private Integer maxNumber;
+
+    public Integer getMaxNumber() {
+        return maxNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+}
---
+++ springdoc-openapi-javadoc/src/test/java/test/org/springdoc/api/app166/SimpleInnerInnerClass.java
@@ -0,0 +1,21 @@
+package test.org.springdoc.api.app166;
+
+public class SimpleInnerInnerClass {
+    /**
+     * Returns the name of the inner inner class
+     */
+    Boolean name;
+
+    /**
+     * Returns the maxNumber of the inner inner class
+     */
+    private Integer maxNumber;
+
+    public Integer getMaxNumber() {
+        return maxNumber;
+    }
+
+    public Boolean getName() {
+        return name;
+    }
+}
