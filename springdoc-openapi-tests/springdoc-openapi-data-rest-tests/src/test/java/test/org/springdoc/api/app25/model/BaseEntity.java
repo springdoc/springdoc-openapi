04d498313d82252b044ad687e70f80a5d4ab18e2
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-08-21 23:06:12 +0200

    code review

--- springdoc-openapi-tests/springdoc-openapi-data-rest-tests/src/test/java/test/org/springdoc/api/app25/model/BaseEntity.java
+++ springdoc-openapi-tests/springdoc-openapi-data-rest-tests/src/test/java/test/org/springdoc/api/app25/model/BaseEntity.java
@@ -9,7 +9,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.Type;
 import org.hibernate.type.SqlTypes;
 
 @Getter
