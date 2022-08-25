888d7538298d4e417d1a12457b8fa7a7597490c0
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-08-25 23:43:15 +0200

    changes report #1816

--- springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app87/HelloController.java
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app87/HelloController.java
@@ -2,21 +2,19 @@
  *
  *  *
  *  *  *
+ *  *  *  * Copyright 2019-2022 the original author or authors.
  *  *  *  *
- *  *  *  *  * Copyright 2019-2022 the original author or authors.
- *  *  *  *  *
- *  *  *  *  * Licensed under the Apache License, Version 2.0 (the "License");
- *  *  *  *  * you may not use this file except in compliance with the License.
- *  *  *  *  * You may obtain a copy of the License at
- *  *  *  *  *
- *  *  *  *  *      https://www.apache.org/licenses/LICENSE-2.0
- *  *  *  *  *
- *  *  *  *  * Unless required by applicable law or agreed to in writing, software
- *  *  *  *  * distributed under the License is distributed on an "AS IS" BASIS,
- *  *  *  *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  *  *  *  * See the License for the specific language governing permissions and
- *  *  *  *  * limitations under the License.
+ *  *  *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  *  * you may not use this file except in compliance with the License.
+ *  *  *  * You may obtain a copy of the License at
  *  *  *  *
+ *  *  *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *  *  *
+ *  *  *  * Unless required by applicable law or agreed to in writing, software
+ *  *  *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  *  * See the License for the specific language governing permissions and
+ *  *  *  * limitations under the License.
  *  *  *
  *  *
  *
@@ -27,12 +25,17 @@ package test.org.springdoc.api.v30.app87;
 import java.util.UUID;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -51,6 +54,19 @@ public class HelloController {
 		return ResponseEntity.ok(item);
 	}
 
+	/**
+	 * List tracker data.
+	 *
+	 * @return the tracker data
+	 */
+	@GetMapping(value = "/values/data")
+	void list(@RequestHeader(value = "access_token", required = false)
+	@Parameter(name = "access_token", in = ParameterIn.HEADER, description = "token in header", schema = @Schema(implementation = String.class))
+	String tokenInHeader,@CookieValue(value = "access_token", required = false)
+	@Parameter(name = "access_token", in = ParameterIn.COOKIE, description = "token in cookie", schema = @Schema(implementation = String.class))
+	String tokenInCookie) {
+
+	}
 	public static class Item {
 	}
 }
