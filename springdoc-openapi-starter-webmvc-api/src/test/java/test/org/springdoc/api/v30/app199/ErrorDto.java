185f5c513388f02a1bbd5c521a1b218430dbcff8
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-12-05 23:39:44 +0100

    override-with-generic-response shouldn't shallow copy. Fixes #1962

---
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app199/ErrorDto.java
@@ -0,0 +1,12 @@
+package test.org.springdoc.api.v30.app199;
+
+public class ErrorDto {
+    private String message;
+
+    public ErrorDto( final String message ) {
+        this.message = message;
+    }
+
+    public String getMessage() { return message; }
+    public void setMessage( final String message ) { this.message = message; }
+}
