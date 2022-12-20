b70b7b70921329e2bb81cef115989d4717c91502
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-12-20 15:02:34 +0100

    Support for nullable request parameters in Kotlin. Fixes #2006

--- springdoc-openapi-tests/springdoc-openapi-kotlin-tests/src/test/kotlin/test/org/springdoc/api/app8/ExampleController.kt
+++ springdoc-openapi-tests/springdoc-openapi-kotlin-tests/src/test/kotlin/test/org/springdoc/api/app8/ExampleController.kt
@@ -1,8 +1,7 @@
-package test.org.springdoc.api.app7
+package test.org.springdoc.api.app8
 
 import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
