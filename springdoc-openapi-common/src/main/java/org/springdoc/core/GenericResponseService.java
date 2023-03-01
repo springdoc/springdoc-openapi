6afecf3370e66f6ecc1a57c1cbc1311ca9c96e52
Yuta Saito <uc4w6c@bma.biglobe.ne.jp>
2023-03-01 23:18:58 +0900

    When getting ExceptionHandler in the controller, use target class in case of AOP Proxy. Fixes #2098

--- springdoc-openapi-common/src/main/java/org/springdoc/core/GenericResponseService.java
+++ springdoc-openapi-common/src/main/java/org/springdoc/core/GenericResponseService.java
@@ -2,7 +2,7 @@
  *
  *  *
  *  *  *
- *  *  *  * Copyright 2019-2022 the original author or authors.
+ *  *  *  * Copyright 2019-2023 the original author or authors.
  *  *  *  *
  *  *  *  * Licensed under the Apache License, Version 2.0 (the "License");
  *  *  *  * you may not use this file except in compliance with the License.
@@ -668,7 +668,12 @@ public class GenericResponseService {
 	 */
 	private synchronized Map<String, ApiResponse> getGenericMapResponse(Class<?> beanType) {
 		List<ControllerAdviceInfo> controllerAdviceInfosInThisBean = localExceptionHandlers.stream()
-				.filter(controllerInfo -> beanType.equals(controllerInfo.getControllerAdvice().getClass()))
+				.filter(controllerInfo -> {
+					Class<?> objClz = controllerInfo.getControllerAdvice().getClass();
+					if (org.springframework.aop.support.AopUtils.isAopProxy(controllerInfo.getControllerAdvice()))
+						objClz = org.springframework.aop.support.AopUtils.getTargetClass(controllerInfo.getControllerAdvice());
+					return beanType.equals(objClz);
+				})
 				.collect(Collectors.toList());
 
 		Map<String, ApiResponse> genericApiResponseMap = controllerAdviceInfosInThisBean.stream()
