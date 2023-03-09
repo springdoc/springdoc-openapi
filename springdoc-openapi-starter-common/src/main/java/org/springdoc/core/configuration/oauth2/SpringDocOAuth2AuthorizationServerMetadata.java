79a9892bd3aba6e5ae95d69ef572eaf670857864
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-03-09 21:02:14 +0100

    fix update #2094

---
+++ springdoc-openapi-starter-common/src/main/java/org/springdoc/core/configuration/oauth2/SpringDocOAuth2AuthorizationServerMetadata.java
@@ -0,0 +1,151 @@
+package org.springdoc.core.configuration.oauth2;
+
+import java.net.URL;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationServerMetadataClaimAccessor;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationServerMetadataClaimNames;
+
+/**
+ * The type Spring doc o auth 2 authorization server metadata.
+ *
+ * @author bnasslahsen
+ */
+@Schema(name = "OAuth2AuthorizationServerMetadata")
+public class SpringDocOAuth2AuthorizationServerMetadata implements OAuth2AuthorizationServerMetadataClaimAccessor {
+
+
+	@Override
+	public Map<String, Object> getClaims() {
+		return null;
+	}
+
+	@Override
+	public <T> T getClaim(String claim) {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getClaim(claim);
+	}
+
+	@Override
+	public boolean hasClaim(String claim) {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.hasClaim(claim);
+	}
+
+	@Override
+	public String getClaimAsString(String claim) {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getClaimAsString(claim);
+	}
+
+	@Override
+	public Boolean getClaimAsBoolean(String claim) {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getClaimAsBoolean(claim);
+	}
+
+	@Override
+	public Instant getClaimAsInstant(String claim) {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getClaimAsInstant(claim);
+	}
+
+	@Override
+	public URL getClaimAsURL(String claim) {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getClaimAsURL(claim);
+	}
+
+	@Override
+	public Map<String, Object> getClaimAsMap(String claim) {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getClaimAsMap(claim);
+	}
+
+	@Override
+	public List<String> getClaimAsStringList(String claim) {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getClaimAsStringList(claim);
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.ISSUER)
+	public URL getIssuer() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getIssuer();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.AUTHORIZATION_ENDPOINT)
+	public URL getAuthorizationEndpoint() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getAuthorizationEndpoint();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.TOKEN_ENDPOINT)
+	public URL getTokenEndpoint() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getTokenEndpoint();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.TOKEN_ENDPOINT_AUTH_METHODS_SUPPORTED)
+	public List<String> getTokenEndpointAuthenticationMethods() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getTokenEndpointAuthenticationMethods();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.JWKS_URI)
+	public URL getJwkSetUrl() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getJwkSetUrl();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.SCOPES_SUPPORTED)
+	public List<String> getScopes() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getScopes();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.RESPONSE_TYPES_SUPPORTED)
+	public List<String> getResponseTypes() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getResponseTypes();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.GRANT_TYPES_SUPPORTED)
+	public List<String> getGrantTypes() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getGrantTypes();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.REVOCATION_ENDPOINT)
+	public URL getTokenRevocationEndpoint() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getTokenRevocationEndpoint();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.REVOCATION_ENDPOINT_AUTH_METHODS_SUPPORTED)
+	public List<String> getTokenRevocationEndpointAuthenticationMethods() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getTokenRevocationEndpointAuthenticationMethods();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.INTROSPECTION_ENDPOINT)
+	public URL getTokenIntrospectionEndpoint() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getTokenIntrospectionEndpoint();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.INTROSPECTION_ENDPOINT_AUTH_METHODS_SUPPORTED)
+	public List<String> getTokenIntrospectionEndpointAuthenticationMethods() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getTokenIntrospectionEndpointAuthenticationMethods();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.REGISTRATION_ENDPOINT)
+	public URL getClientRegistrationEndpoint() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getClientRegistrationEndpoint();
+	}
+
+	@Override
+	@JsonProperty(OAuth2AuthorizationServerMetadataClaimNames.CODE_CHALLENGE_METHODS_SUPPORTED)
+	public List<String> getCodeChallengeMethods() {
+		return OAuth2AuthorizationServerMetadataClaimAccessor.super.getCodeChallengeMethods();
+	}
+}
---
+++ springdoc-openapi-starter-common/src/main/java/org/springdoc/core/configuration/oauth2/SpringDocOAuth2TokenIntrospection.java
@@ -0,0 +1,136 @@
+package org.springdoc.core.configuration.oauth2;
+
+import java.net.URL;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import org.springframework.security.oauth2.core.OAuth2TokenIntrospectionClaimAccessor;
+import org.springframework.security.oauth2.core.OAuth2TokenIntrospectionClaimNames;
+
+/**
+ * @author bnasslahsen
+ */
+@Schema(name = "OAuth2TokenIntrospection")
+public class SpringDocOAuth2TokenIntrospection implements OAuth2TokenIntrospectionClaimAccessor {
+
+	@Override
+	public Map<String, Object> getClaims() {
+		return null;
+	}
+
+	@Override
+	public <T> T getClaim(String claim) {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getClaim(claim);
+	}
+
+	@Override
+	public boolean hasClaim(String claim) {
+		return OAuth2TokenIntrospectionClaimAccessor.super.hasClaim(claim);
+	}
+
+	@Override
+	public String getClaimAsString(String claim) {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getClaimAsString(claim);
+	}
+
+	@Override
+	public Boolean getClaimAsBoolean(String claim) {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getClaimAsBoolean(claim);
+	}
+
+	@Override
+	public Instant getClaimAsInstant(String claim) {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getClaimAsInstant(claim);
+	}
+
+	@Override
+	public URL getClaimAsURL(String claim) {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getClaimAsURL(claim);
+	}
+
+	@Override
+	public Map<String, Object> getClaimAsMap(String claim) {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getClaimAsMap(claim);
+	}
+
+	@Override
+	public List<String> getClaimAsStringList(String claim) {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getClaimAsStringList(claim);
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.ACTIVE)
+	public boolean isActive() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.isActive();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.USERNAME)
+	public String getUsername() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getUsername();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.CLIENT_ID)
+	public String getClientId() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getClientId();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.SCOPE)
+	public List<String> getScopes() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getScopes();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.TOKEN_TYPE)
+	public String getTokenType() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getTokenType();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.EXP)
+	public Instant getExpiresAt() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getExpiresAt();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.IAT)
+	public Instant getIssuedAt() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getIssuedAt();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.NBF)
+	public Instant getNotBefore() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getNotBefore();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.SUB)
+	public String getSubject() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getSubject();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.AUD)
+	public List<String> getAudience() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getAudience();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.ISS)
+	public URL getIssuer() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getIssuer();
+	}
+
+	@Override
+	@JsonProperty(OAuth2TokenIntrospectionClaimNames.JTI)
+	public String getId() {
+		return OAuth2TokenIntrospectionClaimAccessor.super.getId();
+	}
+}
