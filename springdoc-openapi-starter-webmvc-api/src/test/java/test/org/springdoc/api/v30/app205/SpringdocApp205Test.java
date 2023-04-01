7aea84b7894b39da10188f905f534670f2e88cf6
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-04-01 02:00:53 +0200

    Support of {*param} path patterns is broken in 1.6.15. Fixes #2188

---
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app205/SpringdocApp205Test.java
@@ -0,0 +1,21 @@
+package test.org.springdoc.api.v30.app205;
+
+/**
+ * @author bnasslahsen
+ */
+
+import test.org.springdoc.api.v30.AbstractSpringDocV30Test;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.TestPropertySource;
+
+/**
+ * Fix regression in #2031
+ */
+@TestPropertySource(properties = { "springdoc.group-configs[0].group=mygroup", "springdoc.group-configs[0].paths-to-match=/test" })
+public class SpringdocApp205Test extends AbstractSpringDocV30Test {
+
+	@SpringBootApplication
+	static class SpringDocTestApp {}
+
+}
