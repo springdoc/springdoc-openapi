c25eb7c61075398acc86f40987fdc075fb6f6967
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-08-06 00:18:12 +0200

    Group name cannot be null or empty - Fixes #2332

--- springdoc-openapi-starter-common/src/main/java/org/springdoc/core/properties/AbstractSwaggerUiConfigProperties.java
+++ springdoc-openapi-starter-common/src/main/java/org/springdoc/core/properties/AbstractSwaggerUiConfigProperties.java
@@ -32,7 +32,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.commons.lang3.StringUtils;
 
-import static org.springdoc.core.utils.Constants.GROUP_NAME_NOT_NULL;
+import static org.springdoc.core.utils.Constants.GROUP_NAME_NOT_NULL_OR_EMPTY;
 
 /**
  * Please refer to the swagger
@@ -724,7 +724,7 @@ public abstract class AbstractSwaggerUiConfigProperties {
 		 * @param displayName the display name
 		 */
 		public SwaggerUrl(String group, String url, String displayName) {
-			Objects.requireNonNull(group, GROUP_NAME_NOT_NULL);
+			Objects.requireNonNull(group, GROUP_NAME_NOT_NULL_OR_EMPTY);
 			this.url = url;
 			this.name = group;
 			this.displayName = StringUtils.defaultIfEmpty(displayName, this.name);
