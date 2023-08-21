5101b910c85762ba4280991308bda9723776e076
ZHAO JinXiang <xiaoxiangmoe@gmail.com>
2023-08-21 21:55:24 +0800

    Add more test for OpenAPI Specification v3.1

---
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v31/app8/ExamplesResponse.java
@@ -0,0 +1,19 @@
+package test.org.springdoc.api.v31.app8;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.NOT_REQUIRED;
+import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
+
+public record ExamplesResponse(
+        @Schema(description = "self's user info", requiredMode = REQUIRED)
+        UserInfo self,
+        @Schema(description = "friend, deprecated, use friends instead", requiredMode = NOT_REQUIRED)
+        @Deprecated
+        UserInfo friend,
+        @Schema(description = "friends", requiredMode = NOT_REQUIRED)
+        List<UserInfo> friends
+) {
+}
