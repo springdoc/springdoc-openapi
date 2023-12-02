db8b4fda94c0c252f0af402f6acc9da6170cfb87
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-12-02 13:13:56 +0100

    A question about resource path processing. Fixes #2348

--- springdoc-openapi-starter-webflux-ui/src/main/java/org/springdoc/webflux/ui/SwaggerResourceResolver.java
+++ springdoc-openapi-starter-webflux-ui/src/main/java/org/springdoc/webflux/ui/SwaggerResourceResolver.java
@@ -30,23 +30,29 @@ public class SwaggerResourceResolver extends AbstractSwaggerResourceResolver imp
 
 	@Override
 	public Mono<Resource> resolveResource(ServerWebExchange exchange, String requestPath, List<? extends Resource> locations, ResourceResolverChain chain) {
-		Mono<Resource> resolved = chain.resolveResource(exchange, requestPath, locations);
-		if (!Mono.empty().equals(resolved)) {
-			String webJarResourcePath = findWebJarResourcePath(requestPath);
-			if (webJarResourcePath != null)
-				return chain.resolveResource(exchange, webJarResourcePath, locations);
-		}
-		return resolved;
+		return chain.resolveResource(exchange, requestPath, locations)
+				.switchIfEmpty(Mono.defer(() -> {
+					String webJarsResourcePath = findWebJarResourcePath(requestPath);
+					if (webJarsResourcePath != null) {
+						return chain.resolveResource(exchange, webJarsResourcePath, locations);
+					}
+					else {
+						return Mono.empty();
+					}
+				}));
 	}
 
 	@Override
-	public Mono<String> resolveUrlPath(String resourcePath, List<? extends Resource> locations, ResourceResolverChain chain) {
-		Mono<String> path = chain.resolveUrlPath(resourcePath, locations);
-		if (!Mono.empty().equals(path)) {
-			String webJarResourcePath = findWebJarResourcePath(resourcePath);
-			if (webJarResourcePath != null)
-				return chain.resolveUrlPath(webJarResourcePath, locations);
-		}
-		return path;
+	public Mono<String> resolveUrlPath(String resourceUrlPath, List<? extends Resource> locations, ResourceResolverChain chain) {
+		return chain.resolveUrlPath(resourceUrlPath, locations)
+				.switchIfEmpty(Mono.defer(() -> {
+					String webJarResourcePath = findWebJarResourcePath(resourceUrlPath);
+					if (webJarResourcePath != null) {
+						return chain.resolveUrlPath(webJarResourcePath, locations);
+					}
+					else {
+						return Mono.empty();
+					}
+				}));
 	}
 }
