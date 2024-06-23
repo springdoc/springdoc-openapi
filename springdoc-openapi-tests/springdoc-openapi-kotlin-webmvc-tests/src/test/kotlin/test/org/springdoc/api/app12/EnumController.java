524c5fa24593150854ea7ee0fee17cef08caafe1
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-06-23 10:11:08 +0200

    Kotlin enums are always marked as required if used in Java controllers. Fixes #2622

---
+++ springdoc-openapi-tests/springdoc-openapi-kotlin-webmvc-tests/src/test/kotlin/test/org/springdoc/api/app12/EnumController.java
@@ -0,0 +1,18 @@
+package test.org.springdoc.api.app12;
+
+import jakarta.annotation.Nullable;
+import test.org.springdoc.api.app12.SpringDocApp12Test.MyEnum;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author bnasslahsen
+ */
+@RestController
+public class EnumController {
+	@GetMapping("/test-enum-2")
+	String testEnum2(@Nullable MyEnum e) {
+		return "";
+	}
+}
