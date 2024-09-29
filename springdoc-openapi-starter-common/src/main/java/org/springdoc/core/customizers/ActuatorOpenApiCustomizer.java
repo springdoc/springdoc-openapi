c24e9e7b1e8281f2b9ed3770787a66a5b32f0b45
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-09-29 11:29:17 +0200

    Keeping ActuatorOpenApiCustomizer for backward compatibility

---
+++ springdoc-openapi-starter-common/src/main/java/org/springdoc/core/customizers/ActuatorOpenApiCustomizer.java
@@ -0,0 +1,92 @@
+/*
+ *
+ *  *
+ *  *  *
+ *  *  *  *
+ *  *  *  *  * Copyright 2019-2022 the original author or authors.
+ *  *  *  *  *
+ *  *  *  *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  *  *  * you may not use this file except in compliance with the License.
+ *  *  *  *  * You may obtain a copy of the License at
+ *  *  *  *  *
+ *  *  *  *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *  *  *  *
+ *  *  *  *  * Unless required by applicable law or agreed to in writing, software
+ *  *  *  *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  *  *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  *  *  * See the License for the specific language governing permissions and
+ *  *  *  *  * limitations under the License.
+ *  *  *  *
+ *  *  *
+ *  *
+ *
+ */
+
+package org.springdoc.core.customizers;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.PathItem;
+import io.swagger.v3.oas.models.Paths;
+
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+
+/**
+ * The type Actuator open api customiser.
+ * @author bnasslahsen
+ * @Deprecated as not anymore required, use your own {@link org.springdoc.core.customizers.GlobalOpenApiCustomizer} instead
+ */
+public class ActuatorOpenApiCustomizer implements GlobalOpenApiCustomizer {
+
+
+	/**
+	 * The Web endpoint properties.
+	 */
+	private final WebEndpointProperties webEndpointProperties;
+
+	/**
+	 * Instantiates a new Actuator open api customizer.
+	 *
+	 * @param webEndpointProperties the web endpoint properties
+	 */
+	public ActuatorOpenApiCustomizer(WebEndpointProperties webEndpointProperties) {
+		this.webEndpointProperties = webEndpointProperties;
+	}
+
+	private Stream<Entry<String, PathItem>> actuatorPathEntryStream(OpenAPI openApi, String relativeSubPath) {
+		String pathPrefix = webEndpointProperties.getBasePath() + Optional.ofNullable(relativeSubPath).orElse("");
+		return Optional.ofNullable(openApi.getPaths())
+				.map(Paths::entrySet)
+				.map(Set::stream)
+				.map(s -> s.filter(entry -> entry.getKey().startsWith(pathPrefix)))
+				.orElse(Stream.empty());
+	}
+
+	private void handleActuatorOperationIdUniqueness(OpenAPI openApi) {
+		Set<String> usedOperationIds = new HashSet<>();
+		actuatorPathEntryStream(openApi, null)
+				.sorted(Comparator.comparing(Entry::getKey))
+				.forEachOrdered(stringPathItemEntry -> {
+					stringPathItemEntry.getValue().readOperations().forEach(operation -> {
+						String initialOperationId = operation.getOperationId();
+						String uniqueOperationId = operation.getOperationId();
+						int counter = 1;
+						while (!usedOperationIds.add(uniqueOperationId)) {
+							uniqueOperationId = initialOperationId + "_" + ++counter;
+						}
+						operation.setOperationId(uniqueOperationId);
+					});
+				});
+	}
+
+	@Override
+	public void customise(OpenAPI openApi) {
+		handleActuatorOperationIdUniqueness(openApi);
+	}
+}
