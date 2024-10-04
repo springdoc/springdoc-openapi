eb56e6d4d3b4cf4770e51b415e4cfd5707242363
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-10-04 17:06:48 +0200

    Spring Boot (Webflux) - Swagger UI - redirect URI does not include Gateway Prefix. Fixes #2708

---
+++ springdoc-openapi-starter-webflux-ui/src/test/java/test/org/springdoc/ui/app33/SpringDocConfig.java
@@ -0,0 +1,45 @@
+package test.org.springdoc.ui.app33;
+
+import java.util.List;
+
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+
+@Configuration
+public class SpringDocConfig {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringDocConfig.class);
+
+
+	@Bean
+	public WebFilter rewritePathWebFilter(WebFluxProperties webFluxProperties)  {
+		return (ServerWebExchange exchange, WebFilterChain chain) -> {
+			try {
+				ServerHttpRequest originalRequest = (ServerHttpRequest) FieldUtils.readDeclaredField(exchange.getRequest(), "originalRequest", true);
+				List<String> forwardedPrefixHeaders = originalRequest.getHeaders().get("X-Forwarded-Prefix");
+				if (!CollectionUtils.isEmpty(forwardedPrefixHeaders)) {
+					String forwardedPrefix = forwardedPrefixHeaders.get(0);
+					ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+							.contextPath(forwardedPrefix + webFluxProperties.getBasePath())
+							.build();
+					ServerWebExchange mutatedExchange = exchange.mutate().request(mutatedRequest).build();
+					return chain.filter(mutatedExchange);
+				}
+
+			} catch (Exception e) {
+				LOGGER.error(e.getMessage());
+			}
+			return chain.filter(exchange);
+		};
+	}
+}
