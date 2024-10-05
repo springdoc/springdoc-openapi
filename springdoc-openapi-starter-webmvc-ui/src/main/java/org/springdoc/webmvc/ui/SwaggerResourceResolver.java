ab2538e7280d7403d4e9917069d50efaa5f94635
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-10-05 18:26:03 +0200

    Move to webjars-locator-lite, in preparation for spring-boot 3.4 GA. Fixes #2747

--- springdoc-openapi-starter-webmvc-ui/src/main/java/org/springdoc/webmvc/ui/SwaggerResourceResolver.java
+++ springdoc-openapi-starter-webmvc-ui/src/main/java/org/springdoc/webmvc/ui/SwaggerResourceResolver.java
@@ -1,49 +1,46 @@
 package org.springdoc.webmvc.ui;
 
-import java.util.List;
-
-import jakarta.servlet.http.HttpServletRequest;
 import org.springdoc.core.properties.SwaggerUiConfigProperties;
-import org.springdoc.ui.AbstractSwaggerResourceResolver;
 
-import org.springframework.core.io.Resource;
-import org.springframework.web.servlet.resource.ResourceResolver;
-import org.springframework.web.servlet.resource.ResourceResolverChain;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.resource.LiteWebJarsResourceResolver;
+
+import static org.springdoc.ui.SwaggerResourceResolverUtils.findSwaggerResourcePath;
 
 /**
  * The type Web jars version resource resolver.
  *
  * @author bnasslahsen
  */
-public class SwaggerResourceResolver extends AbstractSwaggerResourceResolver implements ResourceResolver {
+public class SwaggerResourceResolver extends LiteWebJarsResourceResolver {
 
+	/**
+	 * The Swagger ui config properties.
+	 */
+	private final SwaggerUiConfigProperties swaggerUiConfigProperties;
+	
 	/**
 	 * Instantiates a new Web jars version resource resolver.
 	 *
 	 * @param swaggerUiConfigProperties the swagger ui config properties
 	 */
 	public SwaggerResourceResolver(SwaggerUiConfigProperties swaggerUiConfigProperties) {
-		super(swaggerUiConfigProperties);
+		this.swaggerUiConfigProperties = swaggerUiConfigProperties;
 	}
 
+	/**
+	 * Find web jar resource path string.
+	 *
+	 * @param pathStr the path
+	 * @return the string
+	 */
+	@Nullable
 	@Override
-	public Resource resolveResource(HttpServletRequest request, String requestPath, List<? extends Resource> locations, ResourceResolverChain chain) {
-		Resource resolved = chain.resolveResource(request, requestPath, locations);
-		if (resolved == null) {
-			String webJarResourcePath = findWebJarResourcePath(requestPath);
-			if (webJarResourcePath != null)
-				return chain.resolveResource(request, webJarResourcePath, locations);
-		}
-		return resolved;	}
-
-	@Override
-	public String resolveUrlPath(String resourcePath, List<? extends Resource> locations, ResourceResolverChain chain) {
-		String path = chain.resolveUrlPath(resourcePath, locations);
-		if (path == null) {
-			String webJarResourcePath = findWebJarResourcePath(resourcePath);
-			if (webJarResourcePath != null)
-				return chain.resolveUrlPath(webJarResourcePath, locations);
-		}
-		return path;
+	protected String findWebJarResourcePath(String pathStr) {
+		String resourcePath = super.findWebJarResourcePath(pathStr);
+		if(resourcePath == null)
+			return findSwaggerResourcePath(pathStr, swaggerUiConfigProperties.getVersion());
+		return resourcePath;
 	}
+
 }
