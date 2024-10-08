428702b2d72726881327a8b154d6baf42da00d96
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2024-10-08 20:48:48 +0200

    Swagger doesn't work after custom annotation replacing request parameters. Fixes #2752

---
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app229/LocaleParam.java
@@ -0,0 +1,66 @@
+/*
+ *
+ *  *
+ *  *  *
+ *  *  *  *
+ *  *  *  *  *
+ *  *  *  *  *  * Copyright 2019-2024 the original author or authors.
+ *  *  *  *  *  *
+ *  *  *  *  *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  *  *  *  *  * you may not use this file except in compliance with the License.
+ *  *  *  *  *  * You may obtain a copy of the License at
+ *  *  *  *  *  *
+ *  *  *  *  *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *  *  *  *  *
+ *  *  *  *  *  * Unless required by applicable law or agreed to in writing, software
+ *  *  *  *  *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  *  *  *  *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  *  *  *  *  * See the License for the specific language governing permissions and
+ *  *  *  *  *  * limitations under the License.
+ *  *  *  *  *
+ *  *  *  *
+ *  *  *
+ *  *
+ *
+ */
+
+package test.org.springdoc.api.v30.app229;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+
+/**
+ * @author bnasslahsen
+ */
+@Target({PARAMETER, METHOD, FIELD, ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Parameter(
+		examples = {
+				@ExampleObject(name = "ar-ae"),
+				@ExampleObject(name = "bg-bg"),
+				@ExampleObject(name = "cs-cz"),
+				@ExampleObject(name = "de-de"),
+				@ExampleObject(name = "el-gr"),
+				@ExampleObject(name = "en-us.src"),
+				@ExampleObject(name = "hu-hu"),
+				@ExampleObject(name = "pl-pl"),
+				@ExampleObject(name = "ro-ro"),
+				@ExampleObject(name = "sk-sk")
+		}
+)
+@Size(min = 2, max = 16)
+@Pattern(regexp = "\\w+([-.]?\\w+)*")
+@Valid
+@interface LocaleParam {}
