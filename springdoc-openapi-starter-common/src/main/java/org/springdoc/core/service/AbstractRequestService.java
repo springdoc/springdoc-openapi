fddac7f01ae4b1f61287afc7fe48bad74c66aee9
Eduard Dudar <eduard.dudar@gmail.com>
2024-10-14 17:44:49 -0700

    Brought back getParameterAnnotation
    ... for reactive cases where method is an implementation and does not carry any annotations but delegate/interface has one

--- springdoc-openapi-starter-common/src/main/java/org/springdoc/core/service/AbstractRequestService.java
+++ springdoc-openapi-starter-common/src/main/java/org/springdoc/core/service/AbstractRequestService.java
@@ -780,6 +780,7 @@ public abstract class AbstractRequestService {
  	 */
 	private boolean checkRequestBodyAnnotation(MethodParameter methodParameter) {
 		return methodParameter.getParameterAnnotation(org.springframework.web.bind.annotation.RequestBody.class) != null
+				|| methodParameter.getParameterAnnotation(io.swagger.v3.oas.annotations.parameters.RequestBody.class) != null
 				|| AnnotatedElementUtils.findMergedAnnotation(Objects.requireNonNull(methodParameter.getParameter()), io.swagger.v3.oas.annotations.parameters.RequestBody.class) != null
 				|| AnnotatedElementUtils.findMergedAnnotation(Objects.requireNonNull(methodParameter.getMethod()), io.swagger.v3.oas.annotations.parameters.RequestBody.class) != null;
 	}
