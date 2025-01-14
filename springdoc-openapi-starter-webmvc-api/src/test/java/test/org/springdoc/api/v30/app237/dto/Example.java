6a45f1eddd22d9f7090fd1fea0c4d17fbfb86c7d
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2025-01-14 20:46:43 +0100

    @JsonUnwrapped is ignored in new version of lib. Fixes #2856

---
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app237/dto/Example.java
@@ -0,0 +1,19 @@
+package test.org.springdoc.api.v30.app237.dto;
+
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record Example(
+        @JsonUnwrapped
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
+        Wrapped unwrapped,
+
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "Some description")
+        Integer number
+) {
+    public record Wrapped(
+            @Schema(requiredMode = Schema.RequiredMode.REQUIRED, description = "Some description of value")
+            String value
+    ) {
+    }
+}
