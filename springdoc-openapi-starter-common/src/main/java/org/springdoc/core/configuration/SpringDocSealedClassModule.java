f4e632b5c51c4a700db02179ec44c9b5b39e022e
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2025-02-16 14:29:51 +0100

    Do not require JsonSubType annotation for sealed classes #2696

--- springdoc-openapi-starter-common/src/main/java/org/springdoc/core/configuration/SpringDocSealedClassModule.java
+++ springdoc-openapi-starter-common/src/main/java/org/springdoc/core/configuration/SpringDocSealedClassModule.java
@@ -22,13 +22,14 @@
 
 package org.springdoc.core.configuration;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import com.fasterxml.jackson.databind.introspect.Annotated;
 import com.fasterxml.jackson.databind.jsontype.NamedType;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import io.swagger.v3.core.jackson.SwaggerAnnotationIntrospector;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * The type Spring doc sealed class module.
@@ -51,7 +52,10 @@ public class SpringDocSealedClassModule extends SimpleModule {
 		public List<NamedType> findSubtypes(Annotated annotated) {
 			ArrayList<NamedType> subTypes = new ArrayList<>();
 
-			if (annotated.getAnnotated() instanceof Class<?> clazz && clazz.isSealed()) {
+			if (annotated.getAnnotated() instanceof Class<?> clazz 
+					&& clazz.isSealed()
+					&& !clazz.getPackage().getName().startsWith("java")
+			) {
 				Class<?>[] permittedSubClasses = clazz.getPermittedSubclasses();
 				if (permittedSubClasses.length > 0) {
 					Arrays.stream(permittedSubClasses).map(NamedType::new).forEach(subTypes::add);
