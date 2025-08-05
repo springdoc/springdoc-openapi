9d6224567d2a0b9047cd37b7a2ca941bf0321759
Mattias-Sehlstedt <60173714+Mattias-Sehlstedt@users.noreply.github.com>
2025-08-05 19:20:44 +0200

    Adjusts the parameter parsing so that a RequestPart with a map is not ignored

--- springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app52/HelloController.java
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app52/HelloController.java
@@ -24,42 +24,52 @@
 
 package test.org.springdoc.api.v30.app52;
 
-import java.util.List;
-
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
-	@PostMapping(value = "/test1/{username}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public String createTest1(@PathVariable String username, @RequestPart("test") MyTestDto test,
-			@RequestPart("image") MultipartFile imageFile) {
-		return null;
-	}
+    @PostMapping(value = "/test1/{username}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String createTest1(
+            @PathVariable String username,
+            @RequestPart("test") MyTestDto test,
+            @RequestPart("image") MultipartFile imageFile) {
+        return null;
+    }
 
-	@PostMapping(value = "/test2/{username}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public String createTest2(@PathVariable String username, @RequestPart("image") MultipartFile imageFile,
-			@RequestPart("test") MyTestDto test, @RequestHeader("My-Header") String workspaceId) {
-		return null;
-	}
+    @PostMapping(value = "/test2/{username}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String createTest2(
+            @PathVariable String username,
+            @RequestPart("image") MultipartFile imageFile,
+            @RequestPart("test") MyTestDto test,
+            @RequestHeader("My-Header") String workspaceId) {
+        return null;
+    }
 
-	@PostMapping(value = "/test3", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public String createTest3(@RequestPart("test") MyTestDto test,
-			@RequestPart("doc") List<MultipartFile> multipartFiles) {
-		return null;
-	}
+    @PostMapping(value = "/test3", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String createTest3(
+            @RequestPart("test") MyTestDto test,
+            @RequestPart("doc") List<MultipartFile> multipartFiles) {
+        return null;
+    }
 
-	class MyTestDto {
-		public String object1;
+    @PostMapping(value = "/test4", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String createTest4(
+            @RequestPart List<MultipartFile> multipartFiles,
+            @RequestPart Map<String, String> map) {
+        return null;
+    }
 
-		public String object2;
+    class MyTestDto {
+        public String object1;
 
-		public String object3;
-	}
+        public String object2;
+
+        public String object3;
+    }
 }
