9bf1ad99d39e9042c57035be861a812292d23aaa
Vasily Pelikh <2010720+vasilypelikh@users.noreply.github.com>
2025-10-27 21:16:47 +0300

    fix tests in springdoc-openapi-starter-webmvc-api

--- springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/AbstractCommonTest.java
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/AbstractCommonTest.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
--- springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v31/app245/SpringDocApp245Test.java
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v31/app245/SpringDocApp245Test.java
@@ -3,7 +3,7 @@ package test.org.springdoc.api.v31.app245;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
