7be8cfe12bb3ee14108abb3cff40b789f78196ff
zakaria-shahen <zakaria-shahen@users.noreply.github.com>
2025-10-30 21:20:30 +0300

    add logs to notify developer the SpringDocs or Scalar is enabled. see #3090

--- springdoc-openapi-starter-webflux-scalar/src/main/java/org/springdoc/webflux/scalar/ScalarConfiguration.java
+++ springdoc-openapi-starter-webflux-scalar/src/main/java/org/springdoc/webflux/scalar/ScalarConfiguration.java
@@ -27,6 +27,8 @@
 package org.springdoc.webflux.scalar;
 
 import com.scalar.maven.webjar.ScalarProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springdoc.core.configuration.SpringDocConfiguration;
 import org.springdoc.core.properties.SpringDocConfigProperties;
 
@@ -40,10 +42,12 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication.Type;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
 
 import static org.springdoc.core.utils.Constants.SPRINGDOC_SWAGGER_UI_ENABLED;
@@ -62,6 +66,14 @@ import static org.springdoc.core.utils.Constants.SPRINGDOC_USE_MANAGEMENT_PORT;
 @EnableConfigurationProperties(ScalarProperties.class)
 @ConditionalOnProperty(prefix = "scalar", name = "enabled", havingValue = "true", matchIfMissing = true)
 public class ScalarConfiguration {
+
+	protected static final Logger LOGGER = LoggerFactory.getLogger(ScalarConfiguration.class);
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void init() {
+		LOGGER.warn("SpringDoc Scalar is enabled by default. To disable it in production, set the property 'scalar.enabled=false' in your production profile configuration.");
+	}
+
 
 	/**
 	 * Scalar web mvc controller scalar web mvc controller.
