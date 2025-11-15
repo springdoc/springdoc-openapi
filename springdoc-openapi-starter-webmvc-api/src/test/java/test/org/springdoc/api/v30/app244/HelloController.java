952d3a8814fc8bd1107c8b9eabd426993443ce08
TAEWOOKK <kto5294@gmail.com>
2025-11-15 20:30:34 +0900

    test: Add tests for EnumDescription on webmvc-api - Field Scanning: Recursively scans DTO classes including parent classes

--- springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app244/HelloController.java
+++ springdoc-openapi-starter-webmvc-api/src/test/java/test/org/springdoc/api/v30/app244/HelloController.java
@@ -26,6 +26,7 @@
 
 package test.org.springdoc.api.v30.app244;
 
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,11 +35,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-	@PostMapping("/test")
-	public void test(@RequestBody TestRequest request) {
-	}
+    @PostMapping("/test")
+    @Operation(description = "Endpoint to request test resource")
+    public void test(@RequestBody TestRequest request) {
+    }
 
-	@GetMapping("/test2")
-	public void test2(TestRequest2 request) {
-	}
+    @GetMapping("/test2")
+    @Operation(description = "Endpoint to request test2 resource")  // ← test2
+    public void test2(TestRequest2 request) {
+    }
+
+    @GetMapping("/test3")
+    @Operation(description = "Endpoint to request test3 resource")  // ← test3
+    public void test3(TestRequest3 request) {
+    }
+
+    @GetMapping("/test4")
+    @Operation(description = "Endpoint to request test4 resource")  // ← test4
+    public void test4(TestRequest4 request) {
+    }
+
+    @PostMapping("/test5")
+    @Operation(description = "Endpoint to request test5 resource")
+    public void test5(@RequestBody TestRequest5 request) {
+    }
 }
