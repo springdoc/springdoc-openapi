81734425f435da39b3b28bcc602cdf0d97758ef3
James Missen <88438228+jamesmissen@users.noreply.github.com>
2025-11-19 03:34:37 +1300

    Add consistent tests for WebFlux and WebMvc to verify correct WebJar mappings

---
+++ springdoc-openapi-starter-webmvc-ui/src/test/java/test/org/springdoc/ui/app38/SpringDocApp38Test.java
@@ -0,0 +1,48 @@
+/*
+ *
+ *  * Copyright 2019-2020 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package test.org.springdoc.ui.app38;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.TestPropertySource;
+import test.org.springdoc.ui.AbstractSpringDocTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestPropertySource(properties = {
+		"spring.mvc.webjars-path-pattern=/webjars-pref/**",
+		"spring.web.resources.add-mappings=false",
+		"springdoc.swagger-ui.path=/documentation/swagger-ui.html"
+})
+public class SpringDocApp38Test extends AbstractSpringDocTest {
+
+	@Test
+	void testWebJarMappingDisabled() throws Exception {
+		mockMvc.perform(get("/webjars/swagger-ui/swagger-initializer.js"))
+				.andExpect(status().isNotFound());
+
+		mockMvc.perform(get("/webjars-pref/swagger-ui/swagger-initializer.js"))
+				.andExpect(status().isNotFound());
+	}
+
+	@SpringBootApplication
+	static class SpringDocTestApp {}
+
+}
