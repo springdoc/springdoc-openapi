a33aaa1b9b56cef3f8a8c87905ff93b4c2029084
Taylore Thornton <tthornton3@chewy.com>
2026-06-25 06:52:12 -0400

    but don't keep nullable if that happens somehow

--- springdoc-openapi-starter-common/src/main/java/org/springdoc/api/AbstractOpenApiResource.java
+++ springdoc-openapi-starter-common/src/main/java/org/springdoc/api/AbstractOpenApiResource.java
@@ -1297,6 +1297,8 @@ public abstract class AbstractOpenApiResource extends SpecFilter {
 					String name = parameter.getName();
 					if (!StringUtils.containsAny(operationPath, "{" + name + "}", "{*" + name + "}"))
 						paramIt.remove();
+					else
+						SpringDocUtils.fixNullablePathParameter(parameter);
 				}
 			}
 		}
